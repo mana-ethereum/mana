5fdcccd7bcf3180f9fdeac699d3420d3837349bc
skovsgaard <bastian.1343@gmail.com>
2014-09-28 19:02:39 +0200

    Removed empty config, updated mix.exs

--- apps/exleveldb/mix.exs
+++ apps/exleveldb/mix.exs
@@ -5,6 +5,8 @@ defmodule Exleveldb.Mixfile do
     [app: :exleveldb,
      version: "0.0.1",
      elixir: "~> 1.0.0",
+     description: description,
+     pacakge: package,
      deps: deps]
   end
 
@@ -27,6 +29,32 @@ defmodule Exleveldb.Mixfile do
   defp deps do
     [
       {:eleveldb, git: "https://github.com/basho/eleveldb", tag: "1.4.0"}
+    ]
+  end
+
+  defp description do
+    """
+    Exleveldb is a thin wrapper around [Basho's eleveldb](https://github.com/basho/eleveldb).
+
+    At the moment, Exleveldb exposes functions for the following features of LevelDB:
+
+    * Opening a new datastore.
+    * Closing an open datastore.
+    * Getting values by key.
+    * Storing individual key-value pairs.
+    * Deleting stored key-value pairs.
+    * Checking if a datastore is empty.
+
+    The idea is to eventually add support for LevelDB's batch operations as well.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README", "LICENSE", "test"],
+      contributors: ["Jonas Skovsgaard Christensen"],
+      licenses: ["Apache v2.0"],
+      linkes: %{"Github" => "https://github.com/skovsgaard/exleveldb.git"}
     ]
   end
 end
