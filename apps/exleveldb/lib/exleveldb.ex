647117fcffe728a049e689eec867e48c777f33b2
skovsgaard <bastian.1343@gmail.com>
2014-09-28 18:41:39 +0200

    Added more tests and is_empty? function

--- apps/exleveldb/lib/exleveldb.ex
+++ apps/exleveldb/lib/exleveldb.ex
@@ -5,8 +5,17 @@ defmodule Exleveldb do
     |> :eleveldb.open opts
   end
 
+  def close(db_ref), do: :eleveldb.close(db_ref)
+
   def get(db_ref, key, opts), do: :eleveldb.get(db_ref, key, opts)
   def put(db_ref, key, val, opts), do: :eleveldb.put(db_ref, key, val, opts)
-  def delete(db_ref, key, opts), do: :eleveldb.delete(db_ref, key, val, opts)
-  
+  def delete(db_ref, key, opts), do: :eleveldb.delete(db_ref, key, opts)
+
+  def is_empty?(db_ref) do
+    if is_atom(:eleveldb.is_empty(db_ref)) do
+      true
+    else
+      false
+    end
+  end
 end
--- apps/exleveldb/test/exleveldb_test.exs
+++ apps/exleveldb/test/exleveldb_test.exs
@@ -1,21 +1,41 @@
 defmodule ExleveldbTest do
   use ExUnit.Case
 
-  def mock_db do
-    File.rm_rf "dbtest"
-    {:ok, mockDb} = Exleveldb.open("dbtest", [{:create_if_missing, :true}])
+  def db_dir do
+    File.mkdir("dbtest")
+    "dbtest"
+  end
+
+  def mock_db(name) do
+    File.rm_rf name
+    {:ok, mockDb} = Exleveldb.open("#{db_dir}/#{name}", [{:create_if_missing, :true}])
     mockDb
   end
 
-  test "it's possible to make a new datastore" do
-    assert File.exists?("dbtest/")
+  test "it's possible to open a new datastore" do
+    assert mock_db("dbtest1") == ""
+    assert File.exists? "dbtest/dbtest1"
   end
 
-  test "it's possible to write to the datastore" do
-    assert Exleveldb.put(mock_db, "test_key", "test val", [{}])
+  test "it's possible to put a key-value pair in the datastore" do
+    assert Exleveldb.put(mock_db("dbtest2"), "test1", "test1 value", []) == :ok
   end
 
-  test "it's possible to retrieve by key" do
-    assert Exleveldb.get(mock_db, "test_key", [{}])
+  test "it's possible to get a value from the datastore by key" do
+    Exleveldb.put(mock_db("dbtest3"), "test2", "test2 value", [])
+    assert Exleveldb.get(mock_db("dbtest3"), "test2", []) == {:ok, "test2 value"}
+  end
+
+  test "it's possible to delete a stored value by key" do
+    Exleveldb.put(mock_db("dbtest4"), "test3", "test3 value", [])
+    assert Exleveldb.delete(mock_db("dbtest4"), "test3", []) == :ok
+  end
+
+  test "it's possible to check if a datastore is empty" do
+    assert Exleveldb.is_empty?(mock_db("dbtest5")) == true
+  end
+
+  test "it's possible to close a currently open datastore" do
+    assert Exleveldb.close(mock_db("dbtest6")) == :ok
   end
 end
