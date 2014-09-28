53f6927206dca62ad7bb3b1d8103a849dd180b03
skovsgaard <bastian.1343@gmail.com>
2014-09-28 16:42:31 +0200

    Added delete and corrected LICENSE for compatbility with eleveldb

--- apps/exleveldb/lib/exleveldb.ex
+++ apps/exleveldb/lib/exleveldb.ex
@@ -5,11 +5,8 @@ defmodule Exleveldb do
     |> :eleveldb.open opts
   end
 
-  def get(db_ref, key, opts) do
-    :eleveldb.get(db_ref, key, opts)
-  end
-
-  def put(db_ref, key, val, opts) do
-    :eleveldb.put(db_ref, key, val, opts)
-  end
+  def get(db_ref, key, opts), do: :eleveldb.get(db_ref, key, opts)
+  def put(db_ref, key, val, opts), do: :eleveldb.put(db_ref, key, val, opts)
+  def delete(db_ref, key, opts), do: :eleveldb.delete(db_ref, key, val, opts)
+  
 end
