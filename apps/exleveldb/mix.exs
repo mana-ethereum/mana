3a61a4297658247ee0e3bf0ba19ef26d91b0bd25
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2014-10-12 18:22:30 +0200

    Attempted to fix mix.exs

--- apps/exleveldb/mix.exs
+++ apps/exleveldb/mix.exs
@@ -14,7 +14,7 @@ defmodule Exleveldb.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:eleveldb]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,7 +28,7 @@ defmodule Exleveldb.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:eleveldb, git: "https://github.com/basho/eleveldb", tag: "1.4.0"}
+      {:eleveldb, github: "basho/eleveldb"}
     ]
   end
 
