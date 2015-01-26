8b67284da34ad151a69b7c08fa421b1b68cd1a02
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-01-26 19:53:45 +0100

    Bumped Elixir v. to 1.0.2

--- apps/exleveldb/mix.exs
+++ apps/exleveldb/mix.exs
@@ -4,7 +4,7 @@ defmodule Exleveldb.Mixfile do
   def project do
     [app: :exleveldb,
      version: "0.1.2",
-     elixir: "~> 1.0.0",
+     elixir: "~> 1.0.2",
      description: description,
      package: package,
      deps: deps]
