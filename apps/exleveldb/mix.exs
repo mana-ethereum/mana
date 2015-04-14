f93b97b3d89bfe2bffa6d0ef10061605623bdfd2
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-04-14 21:46:07 +0200

    Bumped minor version.

--- apps/exleveldb/mix.exs
+++ apps/exleveldb/mix.exs
@@ -3,7 +3,7 @@ defmodule Exleveldb.Mixfile do
 
   def project do
     [app: :exleveldb,
-     version: "0.3.1",
+     version: "0.4.0",
      elixir: "~> 1.0.3",
      description: description,
      package: package,
