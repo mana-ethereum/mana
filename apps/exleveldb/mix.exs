fe312383c5ab150a11dcf48de332d9d75caef4a5
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-04-14 17:28:23 +0200

    Bump patch version

--- apps/exleveldb/mix.exs
+++ apps/exleveldb/mix.exs
@@ -3,7 +3,7 @@ defmodule Exleveldb.Mixfile do
 
   def project do
     [app: :exleveldb,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.0.3",
      description: description,
      package: package,
