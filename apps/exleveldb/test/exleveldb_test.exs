f778a2d7095ee8edece5c00c19b944bf82c49319
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-08-28 19:52:16 +0200

    Rewrite tests to better use ExUnit and handle state

--- apps/exleveldb/test/exleveldb_test.exs
+++ apps/exleveldb/test/exleveldb_test.exs
@@ -1,5 +1,5 @@
 defmodule ExleveldbTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   def db_dir do
     File.mkdir("/tmp/dbtest")
@@ -12,136 +12,132 @@ defmodule ExleveldbTest do
     mockDb
   end
 
-  test "it's possible to open a new datastore" do
-    assert mock_db("dbtest1") == ""
-    assert File.exists? "/tmp/dbtest/dbtest1"
+  setup context do
+    test_db_name = context[:test]
+      |> Atom.to_string
+      |> String.replace(" ", "-")
+
+    on_exit fn ->
+      File.rm_rf test_db_name
+    end
+
+    [db: mock_db(test_db_name), test_location: "#{db_dir}/#{test_db_name}"]
   end
 
-  test "it's possible to put a key-value pair in the datastore" do
-    assert Exleveldb.put(mock_db("dbtest2"), "test1", "test1 value") == :ok
-    assert Exleveldb.put(mock_db("dbtest2_1"), "test1", "test1 value") == :ok
+  test "it's possible to open a new datastore", context do
+    assert context[:db] == "" # Opaque types suck for writing wrappers.
+    assert File.exists? context[:test_location]
   end
 
-  test "it's possible to get a value from the datastore by key" do
-    ref = mock_db("dbtest3")
-    Exleveldb.put(ref, "test2", "test2 value")
-    assert Exleveldb.get(ref, "test2", []) == {:ok, "test2 value"}
+  test "it's possible to put a key-value pair in the datastore", context do
+    assert Exleveldb.put(context[:db], "test1", "test1 value") == :ok
+    assert Exleveldb.put(context[:db], "test1", "test1 value") == :ok
   end
 
-  test "it's possible to delete a stored value by key" do
-    ref  = mock_db("dbtest4")
-    Exleveldb.put(ref, "test3", "test3 value")
-    assert Exleveldb.delete(ref, "test3") == :ok
-    assert Exleveldb.delete(mock_db("dbtest4_1"), "test3") == :ok
+  test "it's possible to get a value from the datastore by key", context do
+    Exleveldb.put(context[:db], "test2", "test2 value")
+    assert Exleveldb.get(context[:db], "test2", []) == {:ok, "test2 value"}
   end
 
-  test "it's possible to check if a datastore is empty" do
-    assert Exleveldb.is_empty?(mock_db("dbtest5")) == true
+  test "it's possible to delete a stored value by key", context do
+    Exleveldb.put(context[:db], "test3", "test3 value")
+    assert Exleveldb.delete(context[:db], "test3") == :ok
+    # Repeat once because it needs to return :ok even when there is no key/value to delete.
+    assert Exleveldb.delete(context[:db], "test3") == :ok
   end
 
-  test "it's possible to close a currently open datastore" do
-    assert Exleveldb.close(mock_db("dbtest6")) == :ok
+  test "it's possible to check if a datastore is empty", context do
+    assert Exleveldb.is_empty?(context[:db]) == true
   end
 
-  test "it's possible to fold over the key-value pairs in the currently open datastore" do
-    File.rm_rf("/tmp/dbtest/dbtest7")
-    {:ok, ref} = Exleveldb.open("/tmp/dbtest/dbtest7", [{:create_if_missing, :true}])
-    Exleveldb.put(ref, "def", "456")
-    Exleveldb.put(ref, "abc", "123")
-    Exleveldb.put(ref, "hij", "789")
+  test "it's possible to close a currently open datastore", context do
+    assert Exleveldb.close(context[:db]) == :ok
+  end
+
+  test "it's possible to fold over the key-value pairs in the currently open datastore", context do
+    Exleveldb.put(context[:db], "def", "456")
+    Exleveldb.put(context[:db], "abc", "123")
+    Exleveldb.put(context[:db], "hij", "789")
     assert [
       {"hij", "789"},
       {"def", "456"},
       {"abc", "123"}
-    ] == Exleveldb.fold(ref, fn({k,v}, acc) -> [{k,v}|acc] end, [])
+    ] == Exleveldb.fold(context[:db], fn({k,v}, acc) -> [{k,v}|acc] end, [])
   end
 
-  test "it's possible to fold over the keys of the currently open datastore" do
-    File.rm_rf("/tmp/dbtest/dbtest8")
-    {:ok, ref} = Exleveldb.open("/tmp/dbtest/dbtest8", [{:create_if_missing, :true}])
-    Exleveldb.put(ref, "def", "456")
-    Exleveldb.put(ref, "abc", "123")
-    Exleveldb.put(ref, "hij", "789")
+  test "it's possible to fold over the keys of the currently open datastore", context do
+    Exleveldb.put(context[:db], "def", "456")
+    Exleveldb.put(context[:db], "abc", "123")
+    Exleveldb.put(context[:db], "hij", "789")
     assert [
       "hij",
       "def",
       "abc"
-    ] == Exleveldb.fold_keys(ref, fn(k, acc) -> [k|acc] end, [])
+    ] == Exleveldb.fold_keys(context[:db], fn(k, acc) -> [k|acc] end, [])
   end
 
-  test "it's possible to map over the key-value pairs in the currently open datastore" do
-    File.rm_rf("/tmp/dbtest/dbtest_map")
-    {:ok, ref} = Exleveldb.open("/tmp/dbtest/dbtest_map")
-    Exleveldb.put(ref, "def", "456")
-    Exleveldb.put(ref, "abc", "123")
-    Exleveldb.put(ref, "hij", "789")
+  test "it's possible to map over the key-value pairs in the currently open datastore", context do
+    Exleveldb.put(context[:db], "def", "456")
+    Exleveldb.put(context[:db], "abc", "123")
+    Exleveldb.put(context[:db], "hij", "789")
     assert [
       {"abc", "123"},
       {"def", "456"},
       {"hij", "789"}
-    ] == Exleveldb.map(ref, &(&1))
+    ] == Exleveldb.map(context[:db], &(&1))
   end
 
-  test "it's possible to map over the keys of the currently open datastore" do
-    File.rm_rf("/tmp/dbtest/dbtest_map_keys")
-    {:ok, ref} = Exleveldb.open("/tmp/dbtest/dbtest_map_keys")
-    Exleveldb.put(ref, "def", "456")
-    Exleveldb.put(ref, "abc", "123")
-    Exleveldb.put(ref, "hij", "789")
+  test "it's possible to map over the keys of the currently open datastore", context do
+    Exleveldb.put(context[:db], "def", "456")
+    Exleveldb.put(context[:db], "abc", "123")
+    Exleveldb.put(context[:db], "hij", "789")
     assert [
       "abc",
       "def",
       "hij"
-    ] == Exleveldb.map_keys(ref, &(&1))
+    ] == Exleveldb.map_keys(context[:db], &(&1))
   end
 
-  test "it's possible to stream key-value pairs from the currently open datastore" do
-    File.rm_rf("/tmp/dbtest/dbtest_stream")
-    {:ok, ref} = Exleveldb.open("/tmp/dbtest/dbtest_stream")
-    Exleveldb.put(ref, "def", "456")
-    Exleveldb.put(ref, "abc", "123")
-    Exleveldb.put(ref, "hij", "789")
+  test "it's possible to stream key-value pairs from the currently open datastore", context do
+    Exleveldb.put(context[:db], "def", "456")
+    Exleveldb.put(context[:db], "abc", "123")
+    Exleveldb.put(context[:db], "hij", "789")
     assert [
       {"abc", "123"},
       {"def", "456"},
       {"hij", "789"}
-    ] == Exleveldb.stream(ref) |> Enum.take(3)
+    ] == Exleveldb.stream(context[:db]) |> Enum.take(3)
   end
 
-  test "it's possible to stream keys from the currently open datastore" do
-    File.rm_rf("/tmp/dbtest/dbtest_stream_keys")
-    {:ok, ref} = Exleveldb.open("/tmp/dbtest/dbtest_stream_keys")
-    Exleveldb.put(ref, "def", "456")
-    Exleveldb.put(ref, "abc", "123")
-    Exleveldb.put(ref, "hij", "789")
+  test "it's possible to stream keys from the currently open datastore", context do
+    Exleveldb.put(context[:db], "def", "456")
+    Exleveldb.put(context[:db], "abc", "123")
+    Exleveldb.put(context[:db], "hij", "789")
     assert [
       "abc",
       "def",
       "hij"
-    ] == Exleveldb.stream(ref, :keys_only) |> Enum.take(3)
+    ] == Exleveldb.stream(context[:db], :keys_only) |> Enum.take(3)
   end
 
-  test "it's possible to perform atomic batch writes" do
-    assert Exleveldb.write(mock_db("dbtest9"), [
+  test "it's possible to perform atomic batch writes", context do
+    assert Exleveldb.write(context[:db], [
       {:put, "a", "1"},
       {:put, "b", "2"},
       {:delete, "a"}
     ]) == :ok
   end
 
-  test "it's possible to destroy a datastore" do
-    File.rm_rf("/tmp/eleveldb.destroy.test")
-    {:ok, ref} = Exleveldb.open("/tmp/eleveldb.destroy.test")
-    :ok = Exleveldb.put(ref,<<"qwe">>,<<"123">>,[])
-    Exleveldb.close(ref)
-    assert Exleveldb.destroy("/tmp/eleveldb.destroy.test",[]) == :ok
-    assert Exleveldb.open("/tmp/eleveldb.destroy.test",[{:error_if_exists, :true}]) != {:ok, ""}
+  test "it's possible to destroy a datastore", context do
+    :ok = Exleveldb.put(context[:db], "qwe", "123", [])
+    Exleveldb.close(context[:db])
+    assert Exleveldb.destroy(context[:test_location], []) == :ok
+    assert Exleveldb.open(context[:test_location], [{:error_if_exists, :true}]) != {:ok, ""}
   end
 
-  test "it's possible to call repair from eleveldb" do
-    {:ok,ref} = Exleveldb.open("/tmp/dbtest10", [{:create_if_missing,:true}])
-    :ok       = Exleveldb.close(ref)
-    assert Exleveldb.repair("/tmp/dbtest10") == :ok
-    Exleveldb.destroy("/tmp/dbtest10")
+  test "it's possible to call repair from eleveldb", context do
+    :ok = Exleveldb.close(context[:db])
+    assert Exleveldb.repair(context[:test_location]) == :ok
+    Exleveldb.destroy(context[:test_location])
   end
 end
