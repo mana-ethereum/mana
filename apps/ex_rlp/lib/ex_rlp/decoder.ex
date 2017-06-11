76c202e20287c58c486d6551b932265eaaf73c5b
Ayrat Badykov <ayratin555@gmail.com>
2017-06-11 20:50:23 +0300

    decode long strings

--- apps/ex_rlp/lib/ex_rlp/decoder.ex
+++ apps/ex_rlp/lib/ex_rlp/decoder.ex
@@ -20,6 +20,15 @@ defmodule ExRLP.Decoder do
     data
   end
 
+  defp decode_item(item) do
+    << be_size_prefix :: binary-size(1), data_with_be :: binary >> = item
+    << be_size_prefix >> = be_size_prefix
+    be_size = be_size_prefix - 183
+    << _prefix :: binary-size(be_size), data :: binary >> = data_with_be
+
+    data
+  end
+
   defp decode_hex(binary) do
     {:ok, decoded_binary} =
       binary
