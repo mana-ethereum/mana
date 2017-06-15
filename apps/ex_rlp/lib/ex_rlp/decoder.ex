3e4eb08dbfc75781993a7fc39163ea84e246aebc
Ayrat Badykov <ayratin555@gmail.com>
2017-06-15 22:38:21 +0300

    decode long lists

--- apps/ex_rlp/lib/ex_rlp/decoder.ex
+++ apps/ex_rlp/lib/ex_rlp/decoder.ex
@@ -7,20 +7,20 @@ defmodule ExRLP.Decoder do
     |> decode_item
   end
 
-  defp decode_item(rlp_binary, result \\ [])
+  defp decode_item(rlp_binary, result \\ [], in_list \\ false)
 
-  defp decode_item("", result) do
+  defp decode_item("", result, in_list) do
     result
   end
 
-  defp decode_item(<< << prefix >>, tail :: binary >>, result) when prefix < 128 do
+  defp decode_item(<< << prefix >>, tail :: binary >>, result, in_list) when prefix < 128 do
     new_item = << prefix >>
 
     new_result = result ++ [new_item]
     decode_item(tail, new_result)
   end
 
-  defp decode_item(<< << prefix >>, tail :: binary >>, result) when prefix <= 183 do
+  defp decode_item(<< << prefix >>, tail :: binary >>, result, in_list) when prefix <= 183 do
     item_length = prefix - 128
 
     << new_item :: binary-size(item_length), new_tail :: binary >> = tail
@@ -29,7 +29,7 @@ defmodule ExRLP.Decoder do
     decode_item(new_tail, new_result)
   end
 
-  defp decode_item(<< << be_size_prefix >>, tail :: binary >>, result) when be_size_prefix <= 192 do
+  defp decode_item(<< << be_size_prefix >>, tail :: binary >>, result, in_list) when be_size_prefix <= 192 do
     be_size = be_size_prefix - 183
 
     << be :: binary-size(be_size), data :: binary >> = tail
@@ -40,13 +40,26 @@ defmodule ExRLP.Decoder do
     decode_item(new_tail, new_result)
   end
 
-  defp decode_item(<< << prefix >>, tail :: binary >>, result) when prefix <= 247 do
+  defp decode_item(<< << prefix >>, tail :: binary >>, result, in_list) when prefix <= 247 do
     list_length = prefix - 192
 
     << list :: binary-size(list_length), new_tail :: binary >> = tail
 
     list_items = decode_item(list)
-    new_result = result ++ list_items
+    list_items = if in_list, do: [list_items], else: list_items
+    new_result = if length(result) == 0, do: list_items, else: result ++ [list_items]
+    decode_item(new_tail, new_result)
+  end
+
+  defp decode_item(<< << be_size_prefix >>, tail :: binary >>, result, in_list) do
+    be_size = be_size_prefix - 247
+
+    << be :: binary-size(be_size), data :: binary >> = tail
+    list_length = be |> :binary.decode_unsigned
+    << list :: binary-size(list_length), new_tail :: binary >> = data
+
+    list_items = decode_item(list, [], true)
+    new_result = if length(result) == 0, do: list_items, else: result ++ [list_items]
     decode_item(new_tail, new_result)
   end
 
