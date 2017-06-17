d70896f81fd4965eab6395339f9d1b30c0d06589
Ayrat Badykov <ayratin555@gmail.com>
2017-06-17 21:24:16 +0300

    remove redundant recursion methods

--- apps/ex_rlp/lib/ex_rlp/decoder.ex
+++ apps/ex_rlp/lib/ex_rlp/decoder.ex
@@ -13,47 +13,35 @@ defmodule ExRLP.Decoder do
     result
   end
 
-  defp decode_item(<< << prefix >>, _tail :: binary >>, nil) when prefix < 128 do
-    << prefix >>
-  end
-
   defp decode_item(<< << prefix >>, tail :: binary >>, result) when prefix < 128 do
     new_item = << prefix >>
 
-    new_result = result ++ [new_item]
-    decode_item(tail, new_result)
-  end
+    new_result = result |> add_new_item(new_item)
 
-  defp decode_item(<< << prefix >>, item :: binary >>, nil) when prefix <= 183 do
-    item
+    decode_item(tail, new_result)
   end
 
   defp decode_item(<< << prefix >>, tail :: binary >>, result) when prefix <= 183 do
     {new_item, new_tail} = decode_medium_binary(prefix, tail, 128)
 
-    new_result = result ++ [new_item]
+    new_result = result |> add_new_item(new_item)
+
     decode_item(new_tail, new_result)
-  end
-
-  defp decode_item(<< << be_size_prefix >>, tail :: binary >>, nil) when be_size_prefix < 192 do
-    {item, _} = decode_long_binary(be_size_prefix, tail, 183)
-
-    item
   end
 
   defp decode_item(<< << be_size_prefix >>, tail :: binary >>, result) when be_size_prefix < 192 do
     {new_item, new_tail} = decode_long_binary(be_size_prefix, tail, 183)
 
-    new_result = result ++ [new_item]
+    new_result = result |> add_new_item(new_item)
+
     decode_item(new_tail, new_result)
   end
 
-  defp decode_item(<< << be_size_prefix >>, _tail :: binary >>, nil) when be_size_prefix == 192 do
-    [[]]
-  end
-
   defp decode_item(<< << be_size_prefix >>, tail :: binary >>, result) when be_size_prefix == 192 do
-    new_result = result ++ [[]]
+    new_item = []
+
+    new_result = result |> add_new_item(new_item)
+
     decode_item(tail, new_result)
   end
 
@@ -61,6 +49,7 @@ defmodule ExRLP.Decoder do
     {list, new_tail} = decode_medium_binary(prefix, tail, 192)
 
     new_result = result |> add_decoded_list(list)
+
     decode_item(new_tail, new_result)
   end
 
@@ -68,7 +57,16 @@ defmodule ExRLP.Decoder do
     {list, new_tail} = decode_long_binary(be_size_prefix, tail, 247)
 
     new_result = result |> add_decoded_list(list)
+
     decode_item(new_tail, new_result)
+  end
+
+  def add_new_item(nil, new_item) do
+    new_item
+  end
+
+  def add_new_item(result, new_item) do
+    result ++ [new_item]
   end
 
   defp add_decoded_list(nil, rlp_list_binary) do
@@ -90,10 +88,10 @@ defmodule ExRLP.Decoder do
 
   defp decode_long_binary(be_size_prefix, tail, prefix) do
     be_size = be_size_prefix - prefix
-
     << be :: binary-size(be_size), data :: binary >> = tail
-    item_size = be |> :binary.decode_unsigned
-    << item :: binary-size(item_size), new_tail :: binary >> = data
+
+    item_length = be |> :binary.decode_unsigned
+    << item :: binary-size(item_length), new_tail :: binary >> = data
 
     {item, new_tail}
   end
