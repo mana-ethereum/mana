d44f80e62bfcf3f567cde7b859ddcbc89bc2327b
Ayrat Badykov <ayratin555@gmail.com>
2017-07-09 19:59:48 +0300

    update branch node

--- apps/merkle_patricia_tree/lib/merkle_patricia_tree/tree.ex
+++ apps/merkle_patricia_tree/lib/merkle_patricia_tree/tree.ex
@@ -12,7 +12,7 @@ defmodule MerklePatriciaTree.Tree do
     new_root =
       root
       |> update_and_delete_storage(key, value, db)
-      |> update_root(db)
+      |> encode_node(db)
 
     %Tree{db: db, root: new_root}
   end
@@ -20,10 +20,10 @@ defmodule MerklePatriciaTree.Tree do
   defp update_and_delete_storage(node, key, value, db) do
     type = node |> node_type
 
-    update_node(node, type, key, value)
+    update_node(node, type, key, value, db)
   end
 
-  defp update_node(_node, :blank, key, value) do
+  defp update_node(_node, :blank, key, value, _db) do
     binary_key =
       key
       |> Nibbles.from_binary
@@ -33,8 +33,19 @@ defmodule MerklePatriciaTree.Tree do
     [binary_key, value]
   end
 
-  defp update_node(node, :leaf, key, value) do
+  defp update_node(node, :branch, "", value, _db) do
+    node |> List.update_at(17, value)
+  end
 
+  defp update_node(node, :branch, [node_key | key_tail], value, db) do
+    new_node_hash =
+      node
+      |> Enum.at(node_key)
+      |> decode_to_node
+      |> update_and_delete_storage(key_tail, value, db)
+      |> encode_node(db)
+
+    node |> List.replace_at(node_key, new_node_hash)
   end
 
   defp decode_to_node("") do
@@ -47,7 +58,7 @@ defmodule MerklePatriciaTree.Tree do
     |> ExRLP.decode
   end
 
-  defp update_root(root, db) do
+  defp encode_node(root, db) do
     rlp_encoding = root |> ExRLP.encode
     sha3_encoding = rlp_encoding |> Utils.keccak
 
