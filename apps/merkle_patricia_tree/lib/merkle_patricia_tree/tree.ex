4a949eb5762302a71d84ee3ccfec02ff4947a501
Ayrat Badykov <ayratin555@gmail.com>
2017-07-16 18:09:55 +0300

    finish sketching leaf node update

--- apps/merkle_patricia_tree/lib/merkle_patricia_tree/tree.ex
+++ apps/merkle_patricia_tree/lib/merkle_patricia_tree/tree.ex
@@ -48,9 +48,10 @@ defmodule MerklePatriciaTree.Tree do
     node |> List.replace_at(node_key, new_node_hash)
   end
 
-  defp update_node([node_key, node_value], :leaf, key, value, db) do
+  defp update_node(node, :leaf, key, value, db) do
     current_key =
-      node_key
+      node
+      |> Enum.at(0)
       |> Nibbles.hex_prefix_decode
       |> Nibbles.remove_terminator
 
@@ -59,43 +60,70 @@ defmodule MerklePatriciaTree.Tree do
     remaining_key = key |> Enum.drop(common_prefix_length)
     current_remaining_key = current_key |> Enum.drop(common_prefix_length)
 
-    new_node = update_leaf_node(node_value, current_remaining_key, remaining_key, value, db)
+    new_node = update_leaf_node(
+      node,
+      current_remaining_key,
+      remaining_key,
+      value,
+      db
+    )
   end
 
-  defp update_leaf_node(node_value, [], [], value, db) do
-    node_value
-    |> decode_to_node
-    |> update_and_delete_storage([], value, db)
+  defp update_leaf_node([node_key | _], [], [], value, db) do
+    [node_key, value]
   end
 
-  defp update_leaf_node(node_value, [], [remaining_key | remaining_tail], value, db) do
+  defp update_leaf_node([node_key | node_value], [key | key_tail], [], value, db) do
     new_node =
       new_branch_node()
-      |> List.replace_at(17, node_value)
+      |> List.replace_at(16, node_value)
 
     remaining_tail_hash =
-      remaining_tail
+      key_tail
       |> Nibbles.add_terminator
       |> Nibbles.hex_prefix_encode
-      |> encode_node(db)
 
-    new_node |> List.replace_at(remaining_key, value)
+    new_leaf_node = [remaining_tail_hash | value] |> encode_node(db)
+
+    new_node |> List.replace_at(key, new_leaf_node)
   end
 
-  defp update_leaf_node(node_value, [cur_key | cur_tail], remaining_key, value, db) do
+  defp update_leaf_node([node_key | node_value], current_remaining_key, [key | key_tail], value, db) do
+    new_node = new_branch_node()
 
+    remaining_key_tail =
+      key_tail
+      |> Nibbles.add_terminator
+      |> Nibbles.hex_prefix_encode
+    new_remaining_node = [remaining_key_tail | node_value] |> encode_node(db)
+    new_node = new_node |> List.replace_at(key, new_remaining_node)
 
+    if length(current_remaining_key) == 0  do
+      new_node |> List.replace_at(16, value)
+    else
+      [cur_key | cur_key_tail] = current_remaining_key
+
+      remaining_cur_key_tail =
+        cur_key_tail
+        |> Nibbles.add_terminator
+        |> Nibbles.hex_prefix_encode
+      new_cur_remaining_node =
+        [remaining_cur_key_tail | value]
+        |> encode_node(db)
+
+      new_node |> List.replace_at(cur_key, new_cur_remaining_node)
+    end
   end
 
   defp update_node(node, :extension, key, value) do
   end
 
-  defp decode_to_node("") do
-    ""
-  end
-
   defp new_branch_node do
     0..16 |> Enum.map(fn(_) -> "" end)
+  end
+
+  defp decode_to_node("") do
+    ""
   end
 
   defp decode_to_node(hash) when byte_size(hash) <= 32 do
