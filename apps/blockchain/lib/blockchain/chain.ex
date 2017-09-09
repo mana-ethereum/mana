1e8be77f20b501185b116bfc754d5b0256b7ce20
Geoffrey Hayes <hayesgm@gmail.com>
2017-09-09 02:28:02 -0700

    Add a complete assertion for the Ropsten chain

--- apps/blockchain/lib/blockchain/chain.ex
+++ apps/blockchain/lib/blockchain/chain.ex
@@ -15,6 +15,7 @@ defmodule Blockchain.Chain do
   defstruct [
     name: nil,
     engine: %{},
+    params: %{},
     genesis: %{},
     nodes: [],
     accounts: %{}
@@ -24,18 +25,26 @@ defmodule Blockchain.Chain do
     name: String.t,
     engine: %{
       String.t => %{
-        params: %{
-          minimumDifficulty: integer(),
-          difficultyBoundDivisor: integer(),
-          durationLimit: integer(),
-          homesteadTransition: integer(),
-          eip150Transition: integer(),
-          eip160Transition: integer(),
-          eip161abcTransition: integer(),
-          eip161dTransition: integer(),
-          maxCodeSize: integer()
-        }
+        minimum_difficulty: integer(),
+        difficulty_bound_divisor: integer(),
+        duration_limit: integer(),
+        homestead_transition: integer(),
+        eip150_transition: integer(),
+        eip160_transition: integer(),
+        eip161abc_transition: integer(),
+        eip161d_transition: integer(),
+        max_code_size: integer()
       }
+    },
+    params: %{
+      gas_limit_bound_divisor: integer(),
+      block_reward: integer(),
+      account_start_nonce: integer(),
+      maximum_extra_data_size: integer(),
+      min_gas_limit: integer(),
+      eip155_transition: integer(),
+      eip98_transition: integer(),
+      eip86_transition: integer(),
     },
     genesis: %{
       difficulty: integer(),
@@ -55,13 +64,30 @@ defmodule Blockchain.Chain do
     }
   }
 
-  @spec load_blockchain(atom()) :: t
-  def load_blockchain(chain) do
+  @doc """
+  Loads a given blockchain, such as Homestead or Ropsten. This
+  chain is used to set the genesis block and tweak parameters
+  of the Blockchain and EVM.
+
+  See the `/chains` directory of this repo for supported
+  block chains.
+
+  ## Examples
+
+      iex> Blockchain.Chain.load_chain(:ropsten).name
+      "Ropsten"
+
+      iex> Blockchain.Chain.load_chain(:ropsten).genesis.difficulty
+      0x100000
+  """
+  @spec load_chain(atom()) :: t
+  def load_chain(chain) do
     chain_data = read_chain!(chain)
 
     %__MODULE__{
       name: chain_data["name"],
       engine: get_engine(chain_data["engine"]),
+      params: get_params(chain_data["params"]),
       genesis: get_genesis(chain_data["genesis"]),
       nodes: chain_data["nodes"],
       accounts: get_accounts(chain_data["accounts"])
@@ -71,33 +97,46 @@ defmodule Blockchain.Chain do
   defp get_engine(engine_map) do
     for {engine, %{"params" => params}} <- engine_map do
       {engine, %{
-        minimumDifficulty: params["minimumDifficulty"] |> load_hex,
-        difficultyBoundDivisor: params["difficultyBoundDivisor"] |> load_hex,
-        durationLimit: params["durationLimit"] |> load_hex,
-        homesteadTransition: params["homesteadTransition"],
-        eip150Transition: params["eip150Transition"],
-        eip160Transition: params["eip160Transition"],
-        eip161abcTransition: params["eip161abcTransition"],
-        eip161dTransition: params["eip161dTransition"],
-        maxCodeSize: params["maxCodeSize"],
+        minimum_difficulty: params["minimumDifficulty"] |> load_hex,
+        difficulty_bound_divisor: params["difficultyBoundDivisor"] |> load_hex,
+        duration_limit: params["durationLimit"] |> load_hex,
+        homestead_transition: params["homesteadTransition"],
+        eip150_transition: params["eip150Transition"],
+        eip160_transition: params["eip160Transition"],
+        eip161abc_transition: params["eip161abcTransition"],
+        eip161d_transition: params["eip161dTransition"],
+        max_code_size: params["maxCodeSize"],
         }}
     end |> Enum.into(%{})
+  end
+
+  defp get_params(params_map) do
+    %{
+      gas_limit_bound_divisor: params_map["gasLimitBoundDivisor"] |> load_hex,
+      block_reward: params_map["blockReward"] |> load_hex,
+      account_start_nonce: params_map["accountStartNonce"] |> load_hex,
+      maximum_extra_data_size: params_map["maximumExtraDataSize"] |> load_hex,
+      min_gas_limit: params_map["minGasLimit"] |> load_hex,
+      eip155_transition: params_map["eip155Transition"],
+      eip98_transition: params_map["eip98Transition"] |> load_hex,
+      eip86_transition: params_map["eip86Transition"] |> load_hex,
+    }
   end
 
   defp get_genesis(genesis_map) do
     %{
       difficulty: genesis_map["difficulty"] |> load_hex,
-      author: genesis_map["author"] |> load_hex,
+      author: genesis_map["author"] |> load_address,
       timestamp: genesis_map["timestamp"] |> load_hex,
-      parentHash: genesis_map["parentHash"] |> load_hex,
-      extraData: genesis_map["extraData"] |> load_hex,
-      gasLimit: genesis_map["gasLimit"] |> load_hex,
+      parent_hash: genesis_map["parentHash"] |> load_raw_hex,
+      extra_data: genesis_map["extraData"] |> load_hex,
+      gas_limit: genesis_map["gasLimit"] |> load_hex,
     }
   end
 
   defp get_accounts(account_map) do
     for {address, account_info} <- account_map do
-      {load_hex(address), %{
+      {load_address(address), %{
         balance: account_info["balance"] |> load_hex,
         nonce: ( if account_info["nonce"], do: account_info["nonce"] |> load_hex, else: 0 ),
       }}
@@ -116,12 +155,18 @@ defmodule Blockchain.Chain do
     "chains/#{Atom.to_string(chain)}.json"
   end
 
-  @spec load_hex(String.t) :: integer()
-  defp load_hex("0x" <> hex_data), do: load_hex(hex_data)
-  defp load_hex(hex_data) when Integer.is_odd(byte_size(hex_data)), do: load_hex("0" <> hex_data)
-  defp load_hex(hex_data) do
-    IO.inspect(hex_data)
-    Base.decode16!(hex_data, case: :lower)
+  @spec load_address(String.t) :: binary()
+  defp load_address(hex_data), do: load_raw_hex(hex_data)
+
+  @spec load_raw_hex(String.t) :: binary()
+  defp load_raw_hex("0x" <> hex_data), do: load_raw_hex(hex_data)
+  defp load_raw_hex(hex_data) when Integer.is_odd(byte_size(hex_data)), do: load_raw_hex("0" <> hex_data)
+  defp load_raw_hex(hex_data) do
+    Base.decode16!(hex_data, case: :mixed)
   end
+
+
+  @spec load_hex(String.t) :: integer()
+  defp load_hex(hex_data), do: hex_data |> load_raw_hex |> :binary.decode_unsigned
 
 end
