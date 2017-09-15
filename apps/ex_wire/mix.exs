688e4aea20d84a340c6cc4bda8fbd14773da049e
Geoffrey Hayes <hayesgm@gmail.com>
2017-09-15 01:33:40 -0700

    Update dependencies

--- apps/ex_wire/mix.exs
+++ apps/ex_wire/mix.exs
@@ -5,9 +5,9 @@ defmodule ExWire.Mixfile do
     [app: :ex_wire,
      version: "0.1.0",
      elixir: "~> 1.4",
-     description: "Elixir Client for RLPx Protocol",
+     description: "Elixir Client for Ethereum's RLPx and DevP2P Protocol",
       package: [
-        maintainers: ["Mason Forest", "Geoffrey Hayes", "Ayrat Badykov"],
+        maintainers: ["Mason Fischer", "Geoffrey Hayes", "Ayrat Badykov"],
         licenses: ["MIT"],
         links: %{"GitHub" => "https://github.com/exthereum/ex_wire"}
       ],
@@ -28,11 +28,10 @@ defmodule ExWire.Mixfile do
   defp deps do
     [
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
-      {:keccakf1600, "~> 2.0.0"},
+      {:keccakf1600, "~> 2.0.0", hex: :keccakf1600_orig},
       {:libsecp256k1, "~> 0.1.2"},
       {:ex_rlp, "~> 0.2.1"},
-      {:blockchain, "~> 0.1.1"},
-      {:evm, "~> 0.1.3"},
+      {:blockchain, "~> 0.1.4"},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
     ]
   end
