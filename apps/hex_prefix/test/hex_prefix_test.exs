de4637463a2f47d5bd47f976ff283d60e0d81804
Ayrat Badykov <ayratin555@gmail.com>
2018-04-15 21:00:34 +0300

    fix credo issues

--- apps/hex_prefix/test/hex_prefix_test.exs
+++ apps/hex_prefix/test/hex_prefix_test.exs
@@ -3,10 +3,10 @@ defmodule HexPrefixTest do
   doctest HexPrefix
 
   test "encode then decode - no terminator" do
-    assert HexPrefix.encode({[0x01, 0x02], false}) |> HexPrefix.decode() == {[1, 2], false}
+    assert {[0x01, 0x02], false} |> HexPrefix.encode() |> HexPrefix.decode() == {[1, 2], false}
   end
 
   test "encode then decode - terminator" do
-    assert HexPrefix.encode({[0x01, 0x02], true}) |> HexPrefix.decode() == {[1, 2], true}
+    assert {[0x01, 0x02], true} |> HexPrefix.encode() |> HexPrefix.decode() == {[1, 2], true}
   end
 end
