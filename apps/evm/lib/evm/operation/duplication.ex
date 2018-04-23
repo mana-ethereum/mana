2ac3605bd717620b8be17fa4dc1da5a22f21b004
Ayrat Badykov <ayratin555@gmail.com>
2018-04-23 19:49:30 +0300

    remove list destructure

--- apps/evm/lib/evm/operation/duplication.ex
+++ apps/evm/lib/evm/operation/duplication.ex
@@ -8,9 +8,12 @@ defmodule EVM.Operation.Duplication do
 
       iex> EVM.Operation.Duplication.dup([1, 2, 3], %{})
       [3, 1, 2, 3]
+
+      iex> EVM.Operation.Duplication.dup([1, 2, 3, 4, 5], %{})
+      [5, 1, 2, 3, 4, 5]
   """
   @spec dup(Operation.stack_args(), Operation.vm_map()) :: Operation.op_result()
-  def dup(list = [head | _], check) do
+  def dup(list, _) do
     last = List.last(list)
 
     [last | list]
