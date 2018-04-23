51a4305e5bc52e5164ac837a3173384a89a17e8f
Ayrat Badykov <ayratin555@gmail.com>
2018-04-23 19:33:39 +0300

    fix duplicatoin operations

--- apps/evm/lib/evm/operation/duplication.ex
+++ apps/evm/lib/evm/operation/duplication.ex
@@ -7,10 +7,12 @@ defmodule EVM.Operation.Duplication do
   ## Examples
 
       iex> EVM.Operation.Duplication.dup([1, 2, 3], %{})
-      [3, 2, 1, 1]
+      [3, 1, 2, 3]
   """
   @spec dup(Operation.stack_args(), Operation.vm_map()) :: Operation.op_result()
-  def dup(list = [head | _], _) do
-    :lists.reverse([head | list])
+  def dup(list = [head | _], check) do
+    last = List.last(list)
+
+    [last | list]
   end
 end
