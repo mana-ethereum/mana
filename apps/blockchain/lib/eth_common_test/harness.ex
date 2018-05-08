b3e88784ee20d886215d96922787040d03d2897d
German Velasco <germsvel@gmail.com>
2018-05-08 13:45:50 -0400

    Use define_common_test/3 instead of eth_test/2
    Block tests were the last to use the `eth_test/2` macro in
    `EthCommmonTest.Harness`. We update this code to use
    `define_common_test/3` instead and we remove `eth_test/2`
    
    Note that `define_common_test/3` and `eth_test/2` are not exactly one to
    one. Since each file under `ethereum_common_test` has a different json
    structure, we have opted to use `define_common_test/3` which simply
    loads the tests by file name and provides the data in the test as a map.
    
    That means that each elixir test (like `block_test.exs`) needs to define
    how to implement a test for each of the "sub-tests" defined within a
    common test file. For example, `GenesisTests/basic_genesis_test.json`
    defines three different sub-tests called `test1`, `test2`, and
    `test3`, so `block_test.exs` needs to loop through each of the three
    sub-tests and decide how to test each of them.

--- apps/blockchain/lib/eth_common_test/harness.ex
+++ apps/blockchain/lib/eth_common_test/harness.ex
@@ -6,7 +6,7 @@ defmodule EthCommonTest.Harness do
   defmacro __using__(_opts) do
     quote do
       import EthCommonTest.Helpers
-      import EthCommonTest.Harness, only: [eth_test: 4, define_common_tests: 3]
+      import EthCommonTest.Harness, only: [define_common_tests: 3]
     end
   end
 
@@ -15,7 +15,7 @@ defmodule EthCommonTest.Harness do
 
     for test_path <- common_tests do
       test_data = EthCommonTest.Helpers.read_test_file(test_path)
-      test_name = Map.keys(test_data) |> hd()
+      test_name = Path.basename(test_path, ".json")
 
       json_data = Poison.encode!(test_data)
 
@@ -24,29 +24,6 @@ defmodule EthCommonTest.Harness do
           data = unquote(json_data) |> Poison.decode!()
 
           unquote(fun).(unquote(test_name), data)
-        end
-      end
-    end
-  end
-
-  defmacro eth_test(test_set, test_subset_or_subsets, tests, fun) do
-    test_subsets = case test_subset_or_subsets do
-      test_subsets when is_list(test_subsets) -> test_subsets
-      test_subset -> [test_subset]
-    end
-
-    for test_subset <- test_subsets do
-      file_name = EthCommonTest.Helpers.test_file_name(test_set, test_subset)
-
-      for {test_name, test} <- EthCommonTest.Helpers.read_test_file(file_name),
-        ( tests == :all or Enum.member?(tests, String.to_atom(test_name)) ) do
-          json = Poison.encode!(test)
-
-        quote do
-          test("#{unquote(test_set)} - #{unquote(test_subset)} - #{unquote(test_name)}", test_params) do
-            test = unquote(json) |> Poison.decode!
-            unquote(fun).(test, unquote(test_subset), unquote(test_name), test_params)
-          end
         end
       end
     end
