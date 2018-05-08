209efff7156d50ed3ae40d7a7b0602cc78655a3b
Ayrat Badykov <ayratin555@gmail.com>
2018-05-08 12:48:03 +0300

    add Bucket.add_node/3 tests

--- apps/ex_wire/lib/ex_wire/struct/bucket.ex
+++ apps/ex_wire/lib/ex_wire/struct/bucket.ex
@@ -48,14 +48,63 @@ defmodule ExWire.Struct.Bucket do
     }
   end
 
+  @doc """
+  Adds a node to bucket. Returns tuple with insert result, inserted node and updated bucket.
 
+  ## Examples
+
+      iex> node1 = ExWire.Struct.Peer.new("13.84.180.240", 30303, "6ce05930c72abc632c58e2e4324f7c7ea478cec0ed4fa2528982cf34483094e9cbc9216e7aa349691242576d552a2a56aaeae426c5303ded677ce455ba1acd9d", time: :test)
+      iex> node2 = ExWire.Struct.Peer.new("13.84.180.140", 30303, "30b7ab30a01c124a6cceca36863ece12c4f5fa68e3ba9b0b51407ccc002eeed3b3102d20a88f1c1d3c3154e2449317b8ef95090e77b312d5cc39354f86d5d606", time: :test)
+      iex> node1
+      ...>   |> ExWire.Struct.Bucket.new(time: :test)
+      ...>   |> ExWire.Struct.Bucket.add_node(node2, time: :test)
+      {:insert_node,
+       %ExWire.Struct.Peer{
+         host: "13.84.180.140",
+         ident: "30b7ab...d5d606",
+         last_seen: 1525704921,
+         port: 30303,
+         remote_id: <<4, 48, 183, 171, 48, 160, 28, 18, 74, 108, 206, 202, 54, 134,
+           62, 206, 18, 196, 245, 250, 104, 227, 186, 155, 11, 81, 64, 124, 204, 0,
+           46, 238, 211, 179, 16, 45, 32, 168, 143, 28, 29, 60, 49, 84, 226, 68, 147,
+           23, 184, 239, 149, 9, 14, 119, 179, 18, 213, 204, 57, 53, 79, 134, 213,
+           214, 6>>
+       },
+       %ExWire.Struct.Bucket{
+         current_node: %ExWire.Struct.Peer{
+           host: "13.84.180.240",
+           ident: "6ce059...1acd9d",
+           last_seen: 1525704921,
+           port: 30303,
+           remote_id: <<4, 108, 224, 89, 48, 199, 42, 188, 99, 44, 88, 226, 228, 50,
+             79, 124, 126, 164, 120, 206, 192, 237, 79, 162, 82, 137, 130, 207, 52,
+             72, 48, 148, 233, 203, 201, 33, 110, 122, 163, 73, 105, 18, 66, 87, 109,
+             85, 42, 42, 86, 170, 234, 228, 38, 197, 48, 61, 237, 103, 124, 228, 85,
+             186, 26, 205, 157>>
+         },
+         nodes: [
+           %ExWire.Struct.Peer{
+             host: "13.84.180.140",
+             ident: "30b7ab...d5d606",
+             last_seen: 1525704921,
+             port: 30303,
+             remote_id: <<4, 48, 183, 171, 48, 160, 28, 18, 74, 108, 206, 202, 54,
+               134, 62, 206, 18, 196, 245, 250, 104, 227, 186, 155, 11, 81, 64, 124,
+               204, 0, 46, 238, 211, 179, 16, 45, 32, 168, 143, 28, 29, 60, 49, 84,
+               226, 68, 147, 23, 184, 239, 149, 9, 14, 119, 179, 18, 213, 204, 57, 53,
+               79, 134, 213, 214, 6>>
+           }
+         ],
+         updated_at: 1525704921
+       }}
+  """
   @spec add_node(t(), Peer.t()) :: {atom, t()}
-  def add_node(bucket = %Bucket{current_node: current_node}, node) do
+  def add_node(bucket = %Bucket{current_node: current_node}, node, options \\ [time: :actual]) do
     cond do
       Peer.equal?(current_node, node) -> {:current_node, nil, bucket}
-      Bucket.member?(bucket, node) -> {:reinsert_node, node, reinsert_node(bucket, node)}
-      Bucket.full?(bucket) -> {:full_bucket, head(bucket), bucket}
-      true -> {:insert_node, node, insert_node(bucket, node)}
+      member?(bucket, node) -> {:reinsert_node, node, reinsert_node(bucket, node, options)}
+      full?(bucket) -> {:full_bucket, head(bucket), bucket}
+      true -> {:insert_node, node, insert_node(bucket, node, options)}
     end
   end
 
@@ -216,10 +265,10 @@ defmodule ExWire.Struct.Bucket do
       iex> head2 == node2
       true
   """
-  def reinsert_node(bucket = %Bucket{}, node) do
+  def reinsert_node(bucket = %Bucket{}, node, options \\ []) do
     bucket
     |> remove_node(node)
-    |> insert_node(node)
+    |> insert_node(node, options)
   end
 
   @doc """
@@ -236,10 +285,9 @@ defmodule ExWire.Struct.Bucket do
       true
   """
   @spec member?(t(), Peer.t()) :: boolean()
-  def member?(%Bucket{nodes: nodes}, node) do
-    nodes
-    |> Enum.any?(fn(bucket_node) ->
-      bucket_node.remote_id == node.remote_id
+  def member?( %Bucket{nodes: nodes},  node) do
+    nodes |> Enum.any?(fn(bucket_node) ->
+      Peer.equal?(bucket_node, node)
     end)
   end
 
@@ -253,6 +301,7 @@ defmodule ExWire.Struct.Bucket do
       iex> bucket |> ExWire.Struct.Bucket.full?()
       false
   """
+  
   @spec full?(t()) :: boolean()
   def full?(%Bucket{nodes: nodes}) do
     Enum.count(nodes) == KademliaConfig.bucket_size()
--- apps/ex_wire/test/ex_wire/struct/bucket_test.exs
+++ apps/ex_wire/test/ex_wire/struct/bucket_test.exs
@@ -1,5 +1,58 @@
 defmodule ExWire.Struct.BucketTest do
   use ExUnit.Case, async: true
+  
+  alias ExWire.Struct.Bucket
+  alias ExWire.KademliaConfig
+  
   doctest ExWire.Struct.Bucket
 
+  describe "add_node/3" do
+    setup do
+      node1 = ExWire.Struct.Peer.new("13.84.180.240", 30303, "6ce05930c72abc632c58e2e4324f7c7ea478cec0ed4fa2528982cf34483094e9cbc9216e7aa349691242576d552a2a56aaeae426c5303ded677ce455ba1acd9d")
+
+      bucket =
+        node1
+        |> ExWire.Struct.Bucket.new(time: :test)
+
+      [bucket: bucket]
+    end
+    
+    test "does not add current_node to bucket", %{bucket: bucket} do
+      current_node = bucket.current_node
+      
+      {:current_node, nil, ^bucket} = bucket |> Bucket.add_node(current_node)
+    end
+
+    test "inserts new node to bucket", %{bucket: bucket} do
+      node = ExWire.Struct.Peer.new("13.84.180.140", 30303, "30b7ab30a01c124a6cceca36863ece12c4f5fa68e3ba9b0b51407ccc002eeed3b3102d20a88f1c1d3c3154e2449317b8ef95090e77b312d5cc39354f86d5d606")
+
+      {:insert_node, ^node, bucket} = bucket |> Bucket.add_node(node)
+
+      assert Bucket.member?(bucket, node)
+    end
+
+    test "reinsert node to bucket", %{bucket: bucket} do
+      node = ExWire.Struct.Peer.new("13.84.180.140", 30303, "30b7ab30a01c124a6cceca36863ece12c4f5fa68e3ba9b0b51407ccc002eeed3b3102d20a88f1c1d3c3154e2449317b8ef95090e77b312d5cc39354f86d5d606")
+
+      {:reinsert_node, ^node, bucket} =
+        bucket
+        |> Bucket.insert_node(node)
+        |> Bucket.add_node(node)
+
+      assert Bucket.member?(bucket, node)
+    end
+
+    test "does not insert node when bucket is full", %{bucket: bucket} do
+      node1 = ExWire.Struct.Peer.new("13.84.180.140", 30303, "30b7ab30a01c124a6cceca36863ece12c4f5fa68e3ba9b0b51407ccc002eeed3b3102d20a88f1c1d3c3154e2449317b8ef95090e77b312d5cc39354f86d5d606")
+      node2 = ExWire.Struct.Peer.new("13.84.181.140", 30303, "20c9ad97c081d63397d7b685a412227a40e23c8bdc6688c6f37e97cfbc22d2b4d1db1510d8f61e6a8866ad7f0e17c02b14182d37ea7c3c8b9c2683aeb6b733a1")
+
+      bucket = 
+        1..KademliaConfig.bucket_size
+        |> Enum.reduce(bucket, fn(_num, acc) ->
+          acc |> Bucket.insert_node(node1)
+        end)
+
+     {:full_bucket, ^node1, ^bucket} = bucket |> Bucket.add_node(node2)
+    end
+  end
 end
