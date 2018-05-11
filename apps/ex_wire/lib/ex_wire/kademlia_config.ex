5d6fe5f0be67686c3d898cd7066ae24ce71b3b96
Ayrat Badykov <ayratin555@gmail.com>
2018-05-11 20:06:28 +0300

    add remaining Kademlia parameters

--- apps/ex_wire/lib/ex_wire/kademlia_config.ex
+++ apps/ex_wire/lib/ex_wire/kademlia_config.ex
@@ -4,7 +4,6 @@ defmodule ExWire.KademliaConfig do
 
   https://pdos.csail.mit.edu/~petar/papers/maymounkov-kademlia-lncs.pdf
   """
-
   # k
   @bucket_size 16
 
@@ -14,13 +13,50 @@ defmodule ExWire.KademliaConfig do
   # key size. 0 <= i < @id_size - number of buckets
   @id_size 256
 
+  # b
+  @bits_per_hop 8
+
+  # in ms
+  @eviction_check_interval 75
+
+  # in ms
+  @request_timeout 300
+
+  # in s
+  @bucket_refresh_interval 3600
+
   @spec bucket_size() :: integer()
   def bucket_size do
     @bucket_size
   end
 
+  @spec concurrency() :: integer()
+  def concurrency do
+    @concurrency
+  end
+
   @spec id_size() :: integer()
   def id_size do
     @id_size
+  end
+
+  @spec bits_per_hop() :: integer()
+  def bits_per_hop do
+    @bits_per_hop
+  end
+
+  @spec eviction_check_interval() :: integer()
+  def eviction_check_interval do
+    @eviction_check_interval
+  end
+
+  @spec request_timeout() :: integer
+  def request_timeout do
+    @request_timeout
+  end
+
+  @spec bucket_refresh_interval() :: integer
+  def bucket_refresh_interval do
+    @bucket_refresh_interval
   end
 end
