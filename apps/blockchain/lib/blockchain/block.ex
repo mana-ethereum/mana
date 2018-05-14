66b664b19fea1410efbc03bf22faa175cb2c38a8
Masha Cernivec <masa179@gmail.com>
2018-05-14 15:03:39 +0100

    The specs were named incorrectly.
    Fixes: Overloaded contract for 'Elixir.Blockchain.Block':check_state_root_validity/3 has overlapping domains.

--- apps/blockchain/lib/blockchain/block.ex
+++ apps/blockchain/lib/blockchain/block.ex
@@ -814,7 +814,7 @@ defmodule Blockchain.Block do
     end
   end
 
-  @spec check_state_root_validity([atom()], t(), t()) :: [atom()]
+  @spec check_ommers_hash_validity([atom()], t(), t()) :: [atom()]
   defp check_ommers_hash_validity(errors, child_block, block) do
     if child_block.header.ommers_hash == block.header.ommers_hash do
       errors
@@ -823,7 +823,7 @@ defmodule Blockchain.Block do
     end
   end
 
-  @spec check_state_root_validity([atom()], t(), t()) :: [atom()]
+  @spec check_transactions_root_validity([atom()], t(), t()) :: [atom()]
   defp check_transactions_root_validity(errors, child_block, block) do
     if child_block.header.transactions_root == block.header.transactions_root do
       errors
@@ -832,7 +832,7 @@ defmodule Blockchain.Block do
     end
   end
 
-  @spec check_state_root_validity([atom()], t(), t()) :: [atom()]
+  @spec check_receipts_root_validity([atom()], t(), t()) :: [atom()]
   defp check_receipts_root_validity(errors, child_block, block) do
     if child_block.header.receipts_root == block.header.receipts_root do
       errors
