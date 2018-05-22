1938fa866fe8e440c503a97dcfba9153ceb9b86a
Ayrat Badykov <ayratin555@gmail.com>
2018-05-22 09:55:18 +0300

    add public key recovery test

--- apps/exth_crypto/test/signature_test.exs
+++ apps/exth_crypto/test/signature_test.exs
@@ -1,4 +1,20 @@
 defmodule ExthCrypto.SignatureTest do
   use ExUnit.Case
   doctest ExthCrypto.Signature
+
+  alias ExthCrypto.Signature
+  alias ExthCrypto.Test
+
+  describe "recover/3" do
+    test "recovers correct public key" do
+      private_key = Test.private_key(:key_a)
+      public_key = Signature.get_public_key(private_key)
+      message = "crypto alchemist"
+
+      {signature, _r, _s, recovery_id} = Signature.sign_digest(message, private_key)
+      recovered_public_key = Signature.recover(message, signature, recovery_id)
+
+      assert recovered_public_key == public_key
+    end
+  end
 end
