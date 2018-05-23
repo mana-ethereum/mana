da785f9b9e898b11f92624951ce491316fa9df18
Ayrat Badykov <ayratin555@gmail.com>
2018-05-23 13:43:14 +0300

    fix failing kademlia process in tests

--- apps/ex_wire/lib/ex_wire/handler.ex
+++ apps/ex_wire/lib/ex_wire/handler.ex
@@ -52,14 +52,18 @@ defmodule ExWire.Handler do
   ## Examples
 
       iex> ExWire.Handler.dispatch(%ExWire.Handler.Params{
-      ...>   remote_host: %ExWire.Struct.Endpoint{ip: [1,2,3,4], udp_port: 55},
-      ...>   signature: 2,
-      ...>   recovery_id: 3,
-      ...>   hash: <<5>>,
-      ...>   data: [1, [<<1,2,3,4>>, <<>>, <<5>>], [<<5,6,7,8>>, <<6>>, <<>>], 4] |> ExRLP.encode(),
-      ...>   timestamp: 123,
-      ...>   type: 1
-      ...> })
+      ...>  remote_host: %ExWire.Struct.Endpoint{ip: [1, 2, 3, 4], udp_port: 55},
+      ...>  signature:
+      ...>    <<193, 30, 149, 122, 226, 192, 230, 158, 118, 204, 173, 80, 63, 232, 67, 152, 216, 249,
+      ...>      89, 52, 162, 92, 233, 201, 177, 108, 63, 120, 152, 134, 149, 220, 73, 198, 29, 93,
+      ...>      218, 123, 50, 70, 8, 202, 17, 171, 67, 245, 70, 235, 163, 158, 201, 246, 223, 114,
+      ...>      168, 7, 7, 95, 9, 53, 165, 8, 177, 13>>,
+      ...>  recovery_id: 1,
+      ...>  hash: <<5>>,
+      ...>  data: [1, [<<1, 2, 3, 4>>, <<>>, <<5>>], [<<5, 6, 7, 8>>, <<6>>, <<>>], 4] |> ExRLP.encode(),
+      ...>  timestamp: 123,
+      ...>  type: 1
+      ...>})
       %ExWire.Message.Pong{
         hash: <<5>>,
         timestamp: 123,
