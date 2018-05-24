196a3bd49dbfad1e2be90d54bca1818447066fd2
Masha Cernivec <masa179@gmail.com>
2018-05-24 17:02:47 +0100

    Add padding to make it 32 bytes

--- apps/evm/lib/evm/builtin.ex
+++ apps/evm/lib/evm/builtin.ex
@@ -46,20 +46,22 @@ defmodule EVM.Builtin do
   ## Examples
 
       iex> EVM.Builtin.run_rip160(3000,  %EVM.ExecEnv{data: <<1, 2, 3>>})
-      {2280, %EVM.SubState{}, %EVM.ExecEnv{data: <<1, 2, 3>>}, <<121, 249, 1, 218,
-        38, 9, 240, 32, 173, 173, 191, 46, 95, 104, 161, 108, 140, 63, 125, 87>>}
+      {2280, %EVM.SubState{}, %EVM.ExecEnv{data: <<1, 2, 3>>},<<0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 121, 249, 1, 218, 38, 9, 240, 32, 173, 173, 191, 46, 95,
+        104, 161, 108, 140, 63, 125, 87>>}
   """
   @spec run_rip160(EVM.Gas.t(), EVM.ExecEnv.t()) ::
           {EVM.Gas.t(), EVM.SubState.t(), EVM.ExecEnv.t(), EVM.VM.output()}
   def run_rip160(gas, exec_env = %EVM.ExecEnv{data: data}) do
     used_gas = @rip160 * MathHelper.bits_to_words(byte_size(data))
+
     if(used_gas < gas) do
       remaining_gas = gas - used_gas
-      result = :crypto.hash(:ripemd160, data)
+      result = :crypto.hash(:ripemd160, data) |> EVM.Helpers.left_pad_bytes(32)
       {remaining_gas, %EVM.SubState{}, exec_env, result}
     else
       {gas, %EVM.SubState{}, exec_env, <<>>}
-   end
+    end
   end
 
   @doc """
