27854f79bd8b73692bef0732fd1171db161e9832
Ayrat Badykov <ayratin555@gmail.com>
2018-05-29 16:13:54 +0300

    start node discovery on kademlia process startup

--- apps/ex_wire/lib/ex_wire/kademlia/server.ex
+++ apps/ex_wire/lib/ex_wire/kademlia/server.ex
@@ -4,6 +4,9 @@ defmodule ExWire.Kademlia.Server do
   @moduledoc false
 
   @default_process_name KademliaState
+  @max_discovery_rounds 7
+  # 5s
+  @discovery_round_period 5 * 1000
 
   alias ExWire.Kademlia.{RoutingTable, Node, Discovery}
 
@@ -11,15 +14,15 @@ defmodule ExWire.Kademlia.Server do
     name = params[:name] || @default_process_name
     network_client_name = Keyword.fetch!(params, :network_client_name)
     current_node = Keyword.fetch!(params, :current_node)
+    nodes = params[:nodes] || []
 
-    GenServer.start_link(__MODULE__, {current_node, network_client_name}, name: name)
+    GenServer.start_link(__MODULE__, {current_node, network_client_name, nodes}, name: name)
   end
 
-  def init({current_node = %Node{}, network_client_name}) do
-    routing_table =
-      current_node
-      |> RoutingTable.new(network_client_name)
-      |> Discovery.start()
+  def init({current_node = %Node{}, network_client_name, nodes}) do
+    routing_table = RoutingTable.new(current_node, network_client_name)
+
+    schedule_discovery_round(0, nodes)
 
     {:ok, %{routing_table: routing_table}}
   end
@@ -56,6 +59,27 @@ defmodule ExWire.Kademlia.Server do
 
   def handle_call(:routing_table, _from, state = %{routing_table: routing_table}) do
     {:reply, routing_table, state}
+  end
+
+  def handle_info({:discovery_round, nodes}, %{routing_table: routing_table}) do
+    updated_table = Discovery.start(routing_table, nodes)
+
+    schedule_discovery_round(updated_table.discovery_round)
+
+    {:noreply, %{routing_table: updated_table}}
+  end
+
+  defp schedule_discovery_round(round, nodes \\ []) do
+    cond do
+      round == 0 ->
+        Process.send_after(self(), {:discovery_round, nodes}, 1_000)
+
+      round <= @max_discovery_rounds ->
+        Process.send_after(self(), {:discovery_round, nodes}, @discovery_round_period)
+
+      true ->
+        :ok
+    end
   end
 
   def handle_call(
