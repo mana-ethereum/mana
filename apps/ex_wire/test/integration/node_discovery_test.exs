1b468c57677e88603fb8c9a204164b9420e5b633
Ayrat Badykov <ayratin555@gmail.com>
2018-05-30 13:32:05 +0300

    add integration for discovery rounds

--- apps/ex_wire/test/integration/node_discovery_test.exs
+++ apps/ex_wire/test/integration/node_discovery_test.exs
@@ -12,6 +12,12 @@ defmodule ExWire.NodeDiscoveryTest do
   @moduletag network: true
 
   @remote_address System.get_env("REMOTE_TEST_PEER") || ExWire.Config.chain().nodes |> List.last()
+  @bootnodes [
+    "enode://6332792c4a00e3e4ee0926ed89e0d27ef985424d97b6a45bf0f23e51f0dcb5e66b875777506458aea7af6f9e4ffb69f43f3778ee73c81ed9d34c51c4b16b0b0f@52.232.243.152:30303",
+    "enode://94c15d1b9e2fe7ce56e458b9a3b672ef11894ddedd0c6f247e0f1d3487f52b66208fb4aeb8179fce6e3a749ea93ed147c37976d67af557508d199d9594c35f09@192.81.208.223:30303",
+    "enode://30b7ab30a01c124a6cceca36863ece12c4f5fa68e3ba9b0b51407ccc002eeed3b3102d20a88f1c1d3c3154e2449317b8ef95090e77b312d5cc39354f86d5d606@52.176.7.10:30303",
+    "enode://865a63255b3bb68023b6bffd5095118fcc13e79dcf014fe4e47e065c350c7cc72af2e53eff895f11ba1bbb6a2b33271c1116ee870f266618eadfc2e78aa7349c@52.176.100.77:30303"
+  ]
 
   setup_all do
     kademlia_process_name = KademliaNodeDiscoveryTest
@@ -26,7 +32,8 @@ defmodule ExWire.NodeDiscoveryTest do
         kademlia_process_name: kademlia_process_name,
         network_adapter: network_adapter,
         supervisor_name: supervisor_name,
-        port: port
+        port: port,
+        nodes: @bootnodes
       ]
     })
 
@@ -64,6 +71,20 @@ defmodule ExWire.NodeDiscoveryTest do
     pings_count = routing_table.expected_pongs |> Map.values() |> Enum.count()
 
     assert pings_count > 0
+  end
+
+  test "successfully finished discovery and adds nodes to routing_table", %{
+    kademlia_name: kademlia_name
+  } do
+    # let's wait for all discovery rounds
+    Process.sleep(30_000)
+
+    nodes_count =
+      Kademlia.routing_table(process_name: kademlia_name).buckets
+      |> Enum.flat_map(fn bucket -> bucket.nodes end)
+      |> Enum.count()
+
+    assert nodes_count > Enum.count(@bootnodes)
   end
 
   defp remote_endpoint do
