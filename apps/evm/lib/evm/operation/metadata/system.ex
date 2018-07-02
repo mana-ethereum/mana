17bc069fc0ad73451eeb680dcd6ac1952739da27
Ayrat Badykov <ayratin555@gmail.com>
2018-07-02 16:09:00 +0300

    skip staticcall

--- apps/evm/lib/evm/operation/metadata/system.ex
+++ apps/evm/lib/evm/operation/metadata/system.ex
@@ -43,14 +43,14 @@ defmodule EVM.Operation.Metadata.System do
                     #   output_count: 1,
                     #   group: :system
                     # },
-                    %{
-                      id: 0xFA,
-                      description: "Static message-call into an account",
-                      sym: :staticcall,
-                      input_count: 6,
-                      output_count: 1,
-                      group: :system
-                    },
+                    # %{
+                    #   id: 0xFA,
+                    #   description: "Static message-call into an account",
+                    #   sym: :staticcall,
+                    #   input_count: 6,
+                    #   output_count: 1,
+                    #   group: :system
+                    # },
                     %{
                       id: 0xFD,
                       description:
