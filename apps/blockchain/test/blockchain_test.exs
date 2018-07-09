2e987481d4620bfd819f2c2f8c63843c293121e9
Ayrat Badykov <ayratin555@gmail.com>
2018-07-09 13:52:37 +0300

    add blocks from json tests

--- apps/blockchain/test/blockchain_test.exs
+++ apps/blockchain/test/blockchain_test.exs
@@ -1,10 +1,11 @@
 defmodule BlockchainTest do
   use ExUnit.Case
   use EthCommonTest.Harness
-  alias Blockchain.{Block, Blocktree}
+
+  alias Blockchain.{Blocktree, Account, Transaction}
   alias MerklePatriciaTree.Trie
-  alias Blockchain.Account
   alias Blockchain.Account.Storage
+  alias Block.Header
 
   doctest Blockchain
 
@@ -15,6 +16,7 @@ defmodule BlockchainTest do
     @test
     |> read_test()
     |> run_test()
+
     # |> assert_state()
   end
 
@@ -28,27 +30,86 @@ defmodule BlockchainTest do
   end
 
   defp run_test(json_test) do
+    state = populate_prestate(json_test)
+    chain = Blockchain.Chain.load_chain(:frontier_test)
+
     blocktree =
       create_blocktree()
-      |> add_genesis_block(json_test)
+      |> add_genesis_block(json_test, state, chain)
 
-    state = populate_prestate(json_test)
-
-    add_blocks(json_test, blocktree, state)
+    # add_blocks(json_test, blocktree, state, chain)
   end
 
-  defp add_genesis_block(blocktree, json_test) do
-    genesis_block =
-      json_test["genesisRLP"]
-      |> maybe_hex()
-      |> ExRLP.decode
-      |> Block.deserialize
+  defp add_genesis_block(blocktree, json_test, state, chain) do
+    genesis_block = block_from_json(json_test["genesisRLP"], json_test["genesisBlockHeader"])
 
-    Blocktree.add_block(blocktree, genesis_block)
+    Blocktree.verify_and_add_block(blocktree, chain, genesis_block, state.db)
   end
 
   defp create_blocktree do
     Blocktree.new_tree()
+  end
+
+  defp add_blocks(json_test, blocktree, state, chain) do
+    Enum.reduce(json_test["blocks"], blocktree, fn json_block, acc ->
+      block =
+        block_from_json(json_block["rlp"], json_block["blockHeader"], json_block["transactions"])
+
+      Blocktree.verify_and_add_block(acc, chain, block, state.db)
+    end)
+  end
+
+  defp block_from_json(rlp, json_header, json_transactions \\ [], json_ommers \\ []) do
+    block = block_from_rlp(rlp)
+    header = header_from_json(json_header)
+    transactions = transactions_from_json(json_transactions)
+
+    %{block | header: header, transactions: transactions, ommers: []}
+  end
+
+  defp block_from_rlp(block_rlp) do
+    block_rlp
+    |> maybe_hex()
+    |> ExRLP.decode()
+    |> Blockchain.Block.deserialize()
+  end
+
+  defp header_from_json(json_header) do
+    %Header{
+      parent_hash: maybe_hex(json_header["parentHash"]),
+      ommers_hash: maybe_hex(json_header["uncleHash"]),
+      beneficiary: maybe_hex(json_header["coinbase"]),
+      state_root: maybe_hex(json_header["stateRoot"]),
+      transactions_root: maybe_hex(json_header["transactionsTrie"]),
+      receipts_root: maybe_hex(json_header["receiptTrie"]),
+      logs_bloom: maybe_hex(json_header["bloom"]),
+      difficulty: load_integer(json_header["difficulty"]),
+      number: load_integer(json_header["number"]),
+      gas_limit: load_integer(json_header["gasLimit"]),
+      gas_used: load_integer(json_header["gasUsed"]),
+      timestamp: load_integer(json_header["timestamp"]),
+      extra_data: maybe_hex(json_header["extraData"]),
+      mix_hash: maybe_hex(json_header["mixHash"]),
+      nonce: maybe_hex(json_header["nonce"])
+    }
+  end
+
+  defp transactions_from_json(json_transactions) do
+    Enum.reduce(json_transactions, [], fn json_transaction, acc ->
+      transaction = %Transaction{
+        nonce: load_integer(json_transaction["nonce"]),
+        gas_price: load_integer(json_transaction["gasPrice"]),
+        gas_limit: load_integer(json_transaction["gasLimit"]),
+        to: maybe_hex(json_transaction["to"]),
+        value: load_integer(json_transaction["value"]),
+        v: maybe_hex(json_transaction["v"]),
+        r: maybe_hex(json_transaction["r"]),
+        s: maybe_hex(json_transaction["s"]),
+        data: maybe_hex(json_transaction["data"])
+      }
+
+      acc ++ [transaction]
+    end)
   end
 
   defp populate_prestate(json_test) do
@@ -59,26 +120,26 @@ defmodule BlockchainTest do
       root_hash: maybe_hex(json_test["genesisBlockHeader"]["stateRoot"])
     }
 
-     Enum.reduce(json_test["pre"], state, fn {address, account}, state ->
-       storage = %Trie{
-       root_hash: Trie.empty_trie_root_hash(),
-       db: db
-     }
+    Enum.reduce(json_test["pre"], state, fn {address, account}, state ->
+      storage = %Trie{
+        root_hash: Trie.empty_trie_root_hash(),
+        db: db
+      }
 
-       storage =
-         Enum.reduce(account["storage"], storage, fn {key, value}, trie ->
-           Storage.put(trie.db, trie.root_hash, load_integer(key), load_integer(value))
-         end)
+      storage =
+        Enum.reduce(account["storage"], storage, fn {key, value}, trie ->
+          Storage.put(trie.db, trie.root_hash, load_integer(key), load_integer(value))
+        end)
 
-       new_account = %Account{
-         nonce: load_integer(account["nonce"]),
-         balance: load_integer(account["balance"]),
-         storage_root: storage.root_hash
-       }
+      new_account = %Account{
+        nonce: load_integer(account["nonce"]),
+        balance: load_integer(account["balance"]),
+        storage_root: storage.root_hash
+      }
 
-       state
-       |> Account.put_account(maybe_hex(address), new_account)
-       |> Account.put_code(maybe_hex(address), maybe_hex(account["code"]))
-     end)
+      state
+      |> Account.put_account(maybe_hex(address), new_account)
+      |> Account.put_code(maybe_hex(address), maybe_hex(account["code"]))
+    end)
   end
 end
