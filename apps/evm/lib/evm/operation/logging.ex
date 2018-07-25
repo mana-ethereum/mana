8ede4fbf8bd81c8cb8b57d427619d356f39c66c6
Ayrat Badykov <ayratin555@gmail.com>
2018-07-25 18:37:51 +0300

    fix doctests

--- apps/evm/lib/evm/operation/logging.ex
+++ apps/evm/lib/evm/operation/logging.ex
@@ -23,22 +23,20 @@ defmodule EVM.Operation.Logging do
       ...> }
       iex> sub_state = %EVM.SubState{logs: [], refund: 0, selfdestruct_list: []}
       iex> vm_map = %{sub_state: sub_state, exec_env: env, machine_state: machine_state}
-      iex> EVM.Operation.Logging.log0([0, 32], vm_map)
-      %{
-        sub_state: %EVM.SubState{
-          logs: [
-            %EVM.LogEntry{
-              address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
-                109, 45, 108, 123, 94, 198>>,
-              data: <<255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255>>,
-              topics: []
-            }
-          ],
-          refund: 0,
-          selfdestruct_list: []
-        }
+      iex> EVM.Operation.Logging.log0([0, 32], vm_map)[:sub_state]
+      %EVM.SubState{
+        logs: [
+          %EVM.LogEntry{
+            address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
+              109, 45, 108, 123, 94, 198>>,
+            data: <<255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+              255, 255, 255, 255, 255>>,
+            topics: []
+          }
+        ],
+        refund: 0,
+        selfdestruct_list: []
       }
   """
   @spec log0(Operation.stack_args(), Operation.vm_map()) :: Operation.op_result()
@@ -69,22 +67,20 @@ defmodule EVM.Operation.Logging do
       iex> sub_state = %EVM.SubState{logs: [], refund: 0, selfdestruct_list: []}
       iex> vm_map = %{sub_state: sub_state, exec_env: env, machine_state: machine_state}
       iex> args = [0, 32, 115792089237316195423570985008687907853269984665640564039457584007913129639935]
-      iex> EVM.Operation.Logging.log1(args, vm_map)
-      %{
-        sub_state: %EVM.SubState{
-          logs: [
-            %EVM.LogEntry{
-              address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
-                109, 45, 108, 123, 94, 198>>,
-              data: <<170, 187, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 204, 221>>,
-              topics: [115792089237316195423570985008687907853269984665640564039457584007913129639935]
-            }
-          ],
-          refund: 0,
-          selfdestruct_list: []
-        }
+      iex> EVM.Operation.Logging.log1(args, vm_map)[:sub_state]
+      %EVM.SubState{
+        logs: [
+          %EVM.LogEntry{
+            address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
+              109, 45, 108, 123, 94, 198>>,
+            data: <<170, 187, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+              255, 255, 255, 204, 221>>,
+            topics: [115792089237316195423570985008687907853269984665640564039457584007913129639935]
+          }
+        ],
+        refund: 0,
+        selfdestruct_list: []
       }
   """
   @spec log1(Operation.stack_args(), Operation.vm_map()) :: Operation.op_result()
@@ -117,24 +113,23 @@ defmodule EVM.Operation.Logging do
       iex> args = [0, 32,
       ...>   115792089237316195423570985008687907853269984665640564039457584007913129639935,
       ...>   115792089237316195423570985008687907853269984665640564039457584007913129639935]
-      iex> EVM.Operation.Logging.log2(args, vm_map)
-      %{
-        sub_state: %EVM.SubState{
-          logs: [
-            %EVM.LogEntry{
-              address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
-                109, 45, 108, 123, 94, 198>>,
-              data: <<170, 187, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 204, 221>>,
-              topics: [115792089237316195423570985008687907853269984665640564039457584007913129639935,
-               115792089237316195423570985008687907853269984665640564039457584007913129639935]
-            }
-          ],
-          refund: 0,
-          selfdestruct_list: []
-        }
+      iex> EVM.Operation.Logging.log2(args, vm_map)[:sub_state]
+      %EVM.SubState{
+        logs: [
+          %EVM.LogEntry{
+            address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
+              109, 45, 108, 123, 94, 198>>,
+            data: <<170, 187, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+              255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+              255, 255, 255, 204, 221>>,
+            topics: [115792089237316195423570985008687907853269984665640564039457584007913129639935,
+             115792089237316195423570985008687907853269984665640564039457584007913129639935]
+          }
+        ],
+        refund: 0,
+        selfdestruct_list: []
       }
+
   """
   @spec log2(Operation.stack_args(), Operation.vm_map()) :: Operation.op_result()
   def log2(args, vm_map) do
@@ -164,20 +159,18 @@ defmodule EVM.Operation.Logging do
       iex> sub_state = %EVM.SubState{logs: [], refund: 0, selfdestruct_list: []}
       iex> vm_map = %{sub_state: sub_state, exec_env: env, machine_state: machine_state}
       iex> args = [1, 0, 0, 0, 0]
-      iex> EVM.Operation.Logging.log3(args, vm_map)
-      %{
-        sub_state: %EVM.SubState{
-          logs: [
-            %EVM.LogEntry{
-              address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
-                109, 45, 108, 123, 94, 198>>,
-              data: "",
-              topics: [0, 0, 0]
-            }
-          ],
-          refund: 0,
-          selfdestruct_list: []
-        }
+      iex> EVM.Operation.Logging.log3(args, vm_map)[:sub_state]
+      %EVM.SubState{
+        logs: [
+          %EVM.LogEntry{
+            address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
+              109, 45, 108, 123, 94, 198>>,
+            data: "",
+            topics: [0, 0, 0]
+          }
+        ],
+        refund: 0,
+        selfdestruct_list: []
       }
   """
   @spec log3(Operation.stack_args(), Operation.vm_map()) :: Operation.op_result()
@@ -212,20 +205,18 @@ defmodule EVM.Operation.Logging do
       iex> vm_map = %{sub_state: sub_state, exec_env: env, machine_state: machine_state}
       iex> args = [115792089237316195423570985008687907853269984665640564039457584007913129639935,
       ...>   1, 0, 0, 0, 0]
-      iex> EVM.Operation.Logging.log4(args, vm_map)
-      %{
-        sub_state: %EVM.SubState{
-          logs: [
-            %EVM.LogEntry{
-              address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
-                109, 45, 108, 123, 94, 198>>,
-              data: <<0>>,
-              topics: [0, 0, 0, 0]
-            }
-          ],
-          refund: 0,
-          selfdestruct_list: []
-        }
+      iex> EVM.Operation.Logging.log4(args, vm_map)[:sub_state]
+      %EVM.SubState{
+        logs: [
+          %EVM.LogEntry{
+            address: <<15, 87, 46, 82, 149, 197, 127, 21, 136, 111, 155, 38, 62, 47,
+              109, 45, 108, 123, 94, 198>>,
+            data: <<0>>,
+            topics: [0, 0, 0, 0]
+          }
+        ],
+        refund: 0,
+        selfdestruct_list: []
       }
   """
   @spec log4(Operation.stack_args(), Operation.vm_map()) :: Operation.op_result()
