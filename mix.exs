bc6ef2f4db1a3387bac5b86d7a7386e56c890751
Ayrat Badykov <ayratin555@gmail.com>
2018-08-13 14:35:59 +0300

    upgrade elixir version

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Exthereum.MixProject do
         :exth_crypto,
         :merkle_patricia_tree
       ],
-      elixir: "~> 1.7.1",
+      elixir: "~> 1.7.2",
       start_permanent: Mix.env() == :prod,
       dialyzer: [
         ignore_warnings: ".dialyzer.ignore-warnings",
