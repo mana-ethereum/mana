20d6361fabc11e34f54908923a1b78d6f27ec3b3
Ayrat Badykov <ayratin555@gmail.com>
2018-08-13 11:27:33 +0300

    choose min between 63/64 and required gas for call operations eip150

--- apps/evm/lib/evm/gas.ex
+++ apps/evm/lib/evm/gas.ex
@@ -142,8 +142,9 @@ defmodule EVM.Gas do
 
     gas_cost = memory_cost + operation_cost
 
-    if machine_state.gas < gas_cost &&
-         !Configuration.fail_nested_operation_lack_of_gas?(exec_env.config) do
+    if Configuration.fail_nested_operation_lack_of_gas?(exec_env.config) do
+      if with_status, do: {:original, gas_cost}, else: gas_cost
+    else
       cost_change_result =
         gas_cost_for_nested_operation(
           operation.sym,
@@ -156,8 +157,6 @@ defmodule EVM.Gas do
         {status, value} -> if with_status, do: {status, value}, else: value
         {status, value, call_gass} -> if with_status, do: {status, value, call_gass}, else: value
       end
-    else
-      if with_status, do: {:original, gas_cost}, else: gas_cost
     end
   end
 
@@ -540,7 +539,11 @@ defmodule EVM.Gas do
         new_call_gas = Helpers.all_but_one_64th(remaining_gas)
         new_gas_cost = new_call_gas + call_cost_without_exec_gas
 
-        {:changed, new_gas_cost, new_call_gas}
+        if new_gas_cost < original_cost do
+          {:changed, new_gas_cost, new_call_gas}
+        else
+          {:original, original_cost}
+        end
       else
         # will fail in EVM.Functions.is_exception_halt?
         {:original, original_cost}
