e41f4899249a46e0ec6c5b9f0e99d1a4ae171bf3
Ayrat Badykov <ayratin555@gmail.com>
2018-08-17 14:07:56 +0300

    add byzantium config

---
+++ apps/evm/lib/evm/configuration/byzantium.ex
@@ -0,0 +1,70 @@
+defmodule EVM.Configuration.Byzantium do
+  defstruct fallback_config: EVM.Configuration.EIP158.new(),
+            has_revert: true
+
+  def new do
+    %__MODULE__{}
+  end
+end
+
+defimpl EVM.Configuration, for: EVM.Configuration.Byzantium do
+  alias EVM.Configuration
+
+  @spec contract_creation_cost(Configuration.t()) :: integer()
+  def contract_creation_cost(config),
+    do: Configuration.contract_creation_cost(config.fallback_config)
+
+  @spec has_delegate_call?(Configuration.t()) :: boolean()
+  def has_delegate_call?(config), do: Configuration.has_delegate_call?(config.fallback_config)
+
+  @spec max_signature_s(Configuration.t()) :: atom()
+  def max_signature_s(config), do: Configuration.max_signature_s(config.fallback_config)
+
+  @spec fail_contract_creation_lack_of_gas?(Configuration.t()) :: boolean()
+  def fail_contract_creation_lack_of_gas?(config),
+    do: Configuration.fail_contract_creation_lack_of_gas?(config.fallback_config)
+
+  @spec extcodesize_cost(Configuration.t()) :: integer()
+  def extcodesize_cost(config), do: Configuration.extcodesize_cost(config.fallback_config)
+
+  @spec extcodecopy_cost(Configuration.t()) :: integer()
+  def extcodecopy_cost(config), do: Configuration.extcodecopy_cost(config.fallback_config)
+
+  @spec balance_cost(Configuration.t()) :: integer()
+  def balance_cost(config), do: Configuration.balance_cost(config.fallback_config)
+
+  @spec sload_cost(Configuration.t()) :: integer()
+  def sload_cost(config), do: Configuration.sload_cost(config.fallback_config)
+
+  @spec call_cost(Configuration.t()) :: integer()
+  def call_cost(config), do: Configuration.call_cost(config.fallback_config)
+
+  @spec selfdestruct_cost(Configuration.t(), keyword()) :: integer()
+  def selfdestruct_cost(config, params),
+    do: Configuration.selfdestruct_cost(config.fallback_config, params)
+
+  @spec fail_nested_operation_lack_of_gas?(Configuration.t()) :: boolean()
+  def fail_nested_operation_lack_of_gas?(config),
+    do: Configuration.fail_nested_operation_lack_of_gas?(config.fallback_config)
+
+  @spec exp_byte_cost(Configuration.t()) :: integer()
+  def exp_byte_cost(config), do: Configuration.exp_byte_cost(config.fallback_config)
+
+  @spec limit_contract_code_size?(Configuration.t(), integer()) :: boolean()
+  def limit_contract_code_size?(config, size),
+    do: Configuration.limit_contract_code_size?(config.fallback_config, size)
+
+  @spec start_nonce(Configuration.t()) :: integer()
+  def start_nonce(config), do: Configuration.start_nonce(config.fallback_config)
+
+  @spec empty_account_value_transfer?(Configuration.t()) :: boolean()
+  def empty_account_value_transfer?(config),
+    do: Configuration.empty_account_value_transfer?(config.fallback_config)
+
+  @spec clean_touched_accounts?(Configuration.t()) :: boolean()
+  def clean_touched_accounts?(config),
+    do: Configuration.clean_touched_accounts?(config.fallback_config)
+
+  @spec has_revert?(Configuration.t()) :: boolean()
+  def has_revert?(config), do: config.has_revert
+end
