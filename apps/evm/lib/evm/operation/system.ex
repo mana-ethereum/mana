c51ce9b18609a36c6e6e0b7df21f2590df691fac
Ayrat Badykov <ayratin555@gmail.com>
2018-09-13 15:58:44 +0300

    fix positional arguments for create2 address generation

--- apps/evm/lib/evm/operation/system.ex
+++ apps/evm/lib/evm/operation/system.ex
@@ -37,7 +37,7 @@ defmodule EVM.Operation.System do
         }
       ) do
     {init_code, _machine_state} = EVM.Memory.read(machine_state, input_offset, input_size)
-    new_account_address = Address.new(exec_env.address, init_code, salt)
+    new_account_address = Address.new(exec_env.address, salt, init_code)
 
     create_account([value, input_offset, input_size], vm_map, new_account_address)
   end
