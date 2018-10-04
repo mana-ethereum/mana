e0efead483f9cf729d1da10376a41b654508951a
German Velasco <germsvel@gmail.com>
2018-10-04 15:15:59 -0400

    Remove large doctest in favor of real test

--- apps/blockchain/lib/blockchain/block_header_info.ex
+++ apps/blockchain/lib/blockchain/block_header_info.ex
@@ -49,24 +49,6 @@ defmodule Blockchain.BlockHeaderInfo do
 
   @doc """
   Returns a block that is `n` blocks before the current block.
-
-  ## Examples
-
-      iex> db = MerklePatriciaTree.Test.random_ets_db()
-      iex> parent_block = %Blockchain.Block{
-      ...>   transactions: [%Blockchain.Transaction{nonce: 5, gas_price: 6, gas_limit: 7, to: <<1::160>>, value: 8, v: 27, r: 9, s: 10, data: "hi"}],
-      ...>   header: %Block.Header{number: 4, parent_hash: <<1, 2, 3>>, beneficiary: <<2, 3, 4>>, difficulty: 100, timestamp: 11, mix_hash: <<1>>, nonce: <<2>>}
-      ...> }
-      iex> block = %Blockchain.Block{
-      ...>   transactions: [],
-      ...>   header: %Block.Header{number: 5, parent_hash: Blockchain.Block.hash(parent_block), beneficiary: <<2, 3, 4>>, difficulty: 100, timestamp: 11, mix_hash: <<1>>, nonce: <<2>> }
-      ...> }
-      iex> Blockchain.Block.put_block(parent_block, db)
-      iex> Blockchain.Block.put_block(block, db)
-      iex> Blockchain.BlockHeaderInfo.new(block.header, db)
-      ...> |> Blockchain.BlockHeaderInfo.get_ancestor_header(1)
-      ...> |> Block.Header.hash()
-      <<225, 215, 217, 9, 148, 107, 83, 180, 84, 178, 164, 158, 204, 244, 31, 54, 216, 217, 104, 231, 118, 243, 114, 255, 227, 229, 130, 79, 152, 251, 199, 47>>
   """
   @impl true
   def get_ancestor_header(_block_header_info, n) when n <= 0 or n > 256, do: nil
