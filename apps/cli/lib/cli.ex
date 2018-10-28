e46a63a3efdbc8f8d7f26dd8f00646d903abc2ea
Geoffrey Hayes <hayesgm@gmail.com>
2018-10-28 11:43:06 -0700

    Alias all the things

--- apps/cli/lib/cli.ex
+++ apps/cli/lib/cli.ex
@@ -15,34 +15,37 @@ defmodule CLI do
   ./mana --sync --rpc --submit-transaction "{...}"
   ```
   """
+  alias Blockchain.{Block, Blocktree, Chain, Genesis}
+  alias MerklePatriciaTree.DB
+  alias MerklePatriciaTree.DB.RockDB
 
   @doc """
   Initiates a sync with a given provider (e.g. a JSON-RPC client, such
   as Infura). This is the basis of our "sync the blockchain" code.
   """
-  def sync(sync_provider \\ CLI.Sync.Infura, provider_args \\ []) do
-    db = MerklePatriciaTree.DB.RocksDB.init(db_name())
-    chain = Blockchain.Chain.load_chain(:foundation)
+  def sync(sync_provider, provider_args \\ []) do
+    db = RocksDB.init(db_name())
+    chain = Chain.load_chain(:foundation)
 
     {:ok, provider_state} = apply(sync_provider, :setup, provider_args)
 
     # First, try to load tree from local database
     tree =
-      case MerklePatriciaTree.DB.get(db, "current_block_tree") do
+      case DB.get(db, "current_block_tree") do
         {:ok, current_block_tree} ->
           :erlang.binary_to_term(current_block_tree)
 
         _ ->
-          Blockchain.Blocktree.new_tree()
+          Blocktree.new_tree()
       end
 
     current_block =
       case tree.best_block do
         nil ->
-          Blockchain.Genesis.create_block(chain, db)
+          Genesis.create_block(chain, db)
 
         block ->
-          {:ok, current_block} = Blockchain.Block.get_block(block.block_hash, db)
+          {:ok, current_block} = Block.get_block(block.block_hash, db)
 
           current_block
       end
