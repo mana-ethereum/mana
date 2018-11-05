11aaa6e87e771ba360d471f2119439539c71d719
German Velasco <germsvel@users.noreply.github.com>
2018-11-05 09:27:21 -0500

    Implement cache calculation for Ethash (#561)
    Implements the cache calculation for Ethash outlined in Appendix J,
    section 3, of the Yellow Paper. Calculation of the `cache` requires an
    initial cache (already implemented) and the RandMemoHash algorithm,
    which is also implemented in this commit.

---
+++ apps/blockchain/test/blockchain/ethash/rand_memo_hash_test.exs
@@ -0,0 +1,27 @@
+defmodule Blockchain.Ethash.RandMemoHashTest do
+  use ExUnit.Case, async: true
+
+  alias Blockchain.Ethash.RandMemoHash
+  alias ExthCrypto.Hash.Keccak
+
+  describe "hash/1" do
+    test "returns the rand memo hash of each element in the list" do
+      element0 = Keccak.kec512(<<0::256>>)
+      element1 = Keccak.kec512(<<1::256>>)
+      input = [element0, element1]
+
+      result = RandMemoHash.hash(input)
+
+      assert result == [
+               <<168, 98, 11, 46, 190, 202, 65, 251, 199, 115, 187, 131, 123, 94, 114, 77, 110,
+                 178, 222, 87, 13, 153, 133, 141, 240, 215, 217, 112, 103, 251, 129, 3, 178, 23,
+                 87, 135, 59, 115, 80, 151, 179, 93, 59, 234, 143, 209, 195, 89, 169, 232, 166,
+                 60, 21, 64, 199, 108, 151, 132, 207, 141, 151, 94, 153, 92>>,
+               <<175, 250, 143, 137, 98, 157, 164, 204, 135, 31, 34, 5, 221, 149, 204, 99, 82,
+                 235, 66, 117, 245, 198, 54, 34, 237, 239, 52, 189, 54, 181, 205, 179, 153, 86,
+                 117, 117, 201, 54, 150, 221, 111, 87, 162, 99, 191, 237, 67, 187, 53, 158, 72,
+                 243, 49, 148, 137, 216, 42, 25, 179, 89, 245, 132, 13, 28>>
+             ]
+    end
+  end
+end
