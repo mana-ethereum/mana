410c23f8a0dea4bed1b533e65cbd1a7cc63502e9
Ayrat Badykov <ayratin555@gmail.com>
2018-11-23 09:09:51 +0300

    add frontier to homestead transition test (#657)
    * add frontier to homestead transition test
    
    * run Homestead tests

--- apps/blockchain/test/blockchain_test.exs
+++ apps/blockchain/test/blockchain_test.exs
@@ -37,7 +37,14 @@ defmodule BlockchainTest do
   @tag :ethereum_common_tests
   @tag :Homestead
   @tag :slow
-  test "runs Homestead blockchain tests", do: run_fork_tests("Homestead")
+  test "runs Homestead blockchain tests",
+    do: run_fork_tests("Homestead")
+
+  @tag :ethereum_common_tests
+  @tag :Homestead
+  @tag :slow
+  test "runs FrontierToHomesteadAt5 blockchain tests",
+    do: run_fork_tests("FrontierToHomesteadAt5")
 
   @tag :ethereum_common_tests
   @tag :HomesteadToDaoAt5
@@ -74,11 +81,6 @@ defmodule BlockchainTest do
   @tag :slow
   test "runs Byzantium To Constantinople transition blockchain tests",
     do: run_fork_tests("ByzantiumToConstantinopleAt5")
-
-  @tag :ethereum_common_tests
-  @tag :FrontierToHomesteadAt5
-  test "runs FrontierToHomesteadAt5 blockchain tests",
-    do: run_fork_tests("FrontierToHomesteadAt5")
 
   @tag :ethereum_common_tests
   @tag :HomesteadToEIP150At5
