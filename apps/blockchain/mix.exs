c7373b047eb2e8f47f38a8031859dcc2011f3b7b
Geoffrey Hayes <hayesgm@gmail.com>
2018-11-28 22:17:09 -0800

    Add Exth Helper App
    Currently, we have a variety of functions (like Ethereum specific helpers for encoding/decoding hex, etc.) that do not have a home, or live in the project where they happened to be first used. This creates a lot of repeat code that ends up being potentially bugging. Here, we create a new app, Exth, which can be used for a variety of shared utilities. Currently, we simply have some debug functions and hex encoders, but we could move in functions from other areas that do not necessarily belong anywhere else. Again, these functions are specific to Ethereum/mana, but not to any other sub-project in particular.
    
    Bump cache
    
    Remove old

--- apps/blockchain/mix.exs
+++ apps/blockchain/mix.exs
@@ -53,6 +53,7 @@ defmodule Blockchain.Mixfile do
       {:ethereumex, "~> 0.5.0"},
       {:evm, in_umbrella: true},
       {:ex_rlp, "~> 0.5.0"},
+      {:exth, in_umbrella: true},
       {:exth_crypto, in_umbrella: true},
       {:merkle_patricia_tree, in_umbrella: true}
     ]
