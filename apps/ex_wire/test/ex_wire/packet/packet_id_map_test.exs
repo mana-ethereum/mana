1c5cba08d8915b60306caa99bcf53f0dfcb00af4
Will Meister <william.meister@consensys.net>
2018-12-10 15:00:19 -0600

    Adding test to show packet count works for Eth 62

--- apps/ex_wire/test/ex_wire/packet/packet_id_map_test.exs
+++ apps/ex_wire/test/ex_wire/packet/packet_id_map_test.exs
@@ -220,5 +220,44 @@ defmodule ExWire.Packet.PacketIdMapTest do
                }
              }
     end
+
+    test "Two Different ETH Capabilities" do
+      map =
+        ExWire.Packet.PacketIdMap.new([
+          ExWire.Packet.Capability.new({"eth", 61}),
+          ExWire.Packet.Capability.new({"eth", 62})
+        ])
+
+      assert map == %ExWire.Packet.PacketIdMap{
+               ids_to_modules: %{
+                 0x00 => ExWire.Packet.Protocol.Hello,
+                 0x01 => ExWire.Packet.Protocol.Disconnect,
+                 0x02 => ExWire.Packet.Protocol.Ping,
+                 0x03 => ExWire.Packet.Protocol.Pong,
+                 0x10 => ExWire.Packet.Capability.Eth.Status,
+                 0x11 => ExWire.Packet.Capability.Eth.NewBlockHashes,
+                 0x12 => ExWire.Packet.Capability.Eth.Transactions,
+                 0x13 => ExWire.Packet.Capability.Eth.GetBlockHeaders,
+                 0x14 => ExWire.Packet.Capability.Eth.BlockHeaders,
+                 0x15 => ExWire.Packet.Capability.Eth.GetBlockBodies,
+                 0x16 => ExWire.Packet.Capability.Eth.BlockBodies,
+                 0x17 => ExWire.Packet.Capability.Eth.NewBlock
+               },
+               modules_to_ids: %{
+                 ExWire.Packet.Protocol.Hello => 0x00,
+                 ExWire.Packet.Protocol.Disconnect => 0x01,
+                 ExWire.Packet.Protocol.Ping => 0x02,
+                 ExWire.Packet.Protocol.Pong => 0x03,
+                 ExWire.Packet.Capability.Eth.Status => 0x10,
+                 ExWire.Packet.Capability.Eth.NewBlockHashes => 0x11,
+                 ExWire.Packet.Capability.Eth.Transactions => 0x12,
+                 ExWire.Packet.Capability.Eth.GetBlockHeaders => 0x13,
+                 ExWire.Packet.Capability.Eth.BlockHeaders => 0x14,
+                 ExWire.Packet.Capability.Eth.GetBlockBodies => 0x15,
+                 ExWire.Packet.Capability.Eth.BlockBodies => 0x16,
+                 ExWire.Packet.Capability.Eth.NewBlock => 0x17
+               }
+             }
+    end
   end
 end
