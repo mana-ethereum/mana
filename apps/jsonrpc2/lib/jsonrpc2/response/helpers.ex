c6cd4872117ce7ee39919a2e5a12e29cff30fdd3
Ayrat Badykov <ayratin555@gmail.com>
2018-12-10 18:29:04 +0300

    encode all block fields for JSON RPC (#715)

--- apps/jsonrpc2/lib/jsonrpc2/response/helpers.ex
+++ apps/jsonrpc2/lib/jsonrpc2/response/helpers.ex
@@ -1,13 +1,19 @@
 defmodule JSONRPC2.Response.Helpers do
-  @spec encode_hex(binary() | nil) :: binary()
+  @spec encode_hex(binary() | integer() | nil) :: binary()
   def encode_hex(binary) when is_binary(binary) do
     Exth.encode_hex(binary)
+  end
+
+  def encode_hex(value) when is_integer(value) do
+    value
+    |> :binary.encode_unsigned()
+    |> encode_hex()
   end
 
   def encode_hex(nil), do: nil
 
   def encode_hex(_value) do
     raise ArgumentError,
-      message: "JSONRPC2.Response.Helpers.encode_hex/1 can only encode binary values"
+      message: "JSONRPC2.Response.Helpers.encode_hex/1 can only encode binary and integer values"
   end
 end
