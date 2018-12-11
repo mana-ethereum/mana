b0fcf839564f30f332204d15518cd577b78e8a1f
Ayrat Badykov <ayratin555@gmail.com>
2018-12-11 19:33:08 +0300

    add transaction getter methods

--- apps/jsonrpc2/lib/bridge/sync.ex
+++ apps/jsonrpc2/lib/bridge/sync.ex
@@ -28,20 +28,46 @@ defmodule JSONRPC2.Bridge.Sync do
   @spec get_last_sync_state() :: Sync.state()
   defp get_last_sync_state(), do: Sync.get_state()
 
-  def get_block_by_number(number, full_transactions) do
+  def get_block_by_number(number) do
     state_trie = get_last_sync_state().trie
 
     case Block.get_block_by_number(number, state_trie) do
-      {:ok, block} -> ResponseBlock.new(block, full_transactions)
+      {:ok, block} -> ResponseBlock.new(block)
       _ -> nil
     end
   end
 
-  def get_block_by_hash(hash, full_transactions) do
+  def get_block_by_hash(hash) do
     state_trie = get_last_sync_state().trie
 
     case Block.get_block(hash, state_trie) do
-      {:ok, block} -> ResponseBlock.new(block, full_transactions)
+      {:ok, block} -> ResponseBlock.new(block)
+      _ -> nil
+    end
+  end
+
+  def get_transaction_by_block_hash_and_index(block_hash, trx_index) do
+    trie = get_last_sync_state().trie
+
+    with {:ok, block} <- Block.get_block(block_hash, trie) do
+      case Enum.at(block.transactions, trx_index) do
+        nil -> nil
+        transaction -> ResponseTransaction.new(transaction, block)
+      end
+    else
+      _ -> nil
+    end
+  end
+
+  def get_transaction_by_block_number_and_index(block_number, trx_index) do
+    trie = get_last_sync_state().trie
+
+    with {:ok, block} <- Block.get_block_by_number(block_number, trie) do
+      case Enum.at(block.transactions, trx_index) do
+        nil -> nil
+        transaction -> ResponseTransaction.new(transaction, block)
+      end
+    else
       _ -> nil
     end
   end
