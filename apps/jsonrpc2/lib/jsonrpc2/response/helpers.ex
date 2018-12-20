d47ab76d68917899a34c3c1c30aa15c44bcd52e3
Ayrat Badykov <ayratin555@gmail.com>
2018-12-20 19:54:28 +0300

    encode hex values according to Ethereum JSON RPC spec (#736)
    * encode hex values according to Ethereum JSON RPC spec
    
    * format hex encoded values according to Ethereum JSON RPC spec
    
    * add deleted tests
    
    * fix dialyzer
    
    * remove redundant case
    
    * fix tests
    
    * fix dialyzer

--- apps/jsonrpc2/lib/jsonrpc2/response/helpers.ex
+++ apps/jsonrpc2/lib/jsonrpc2/response/helpers.ex
@@ -1,19 +1,34 @@
 defmodule JSONRPC2.Response.Helpers do
-  @spec encode_hex(binary() | integer() | nil) :: binary()
-  def encode_hex(binary) when is_binary(binary) do
+  @moduledoc """
+  Encodes an usigned integer as a binary as described in Ethereum JSON RPC spec.
+  """
+  @spec encode_quantity(nil | binary() | non_neg_integer()) :: nil | <<_::24>>
+  def encode_quantity(binary) when is_binary(binary) do
+    hex_binary = Base.encode16(binary, case: :lower)
+
+    result = String.replace_leading(hex_binary, "0", "")
+
+    result = if result == "", do: "0", else: result
+
+    "0x#{result}"
+  end
+
+  def encode_quantity(value) when is_integer(value) do
+    value
+    |> :binary.encode_unsigned()
+    |> encode_quantity()
+  end
+
+  def encode_quantity(value) when is_nil(value) do
+    nil
+  end
+
+  @spec encode_unformatted_data(binary()) :: binary() | nil
+  def encode_unformatted_data(binary) when is_binary(binary) do
     Exth.encode_hex(binary)
   end
 
-  def encode_hex(value) when is_integer(value) do
-    value
-    |> :binary.encode_unsigned()
-    |> encode_hex()
-  end
-
-  def encode_hex(nil), do: nil
-
-  def encode_hex(_value) do
-    raise ArgumentError,
-      message: "JSONRPC2.Response.Helpers.encode_hex/1 can only encode binary and integer values"
+  def encode_unformatted_data(binary) when is_nil(binary) do
+    nil
   end
 end
--- apps/jsonrpc2/test/jsonrpc2/response/transaction_test.exs
+++ apps/jsonrpc2/test/jsonrpc2/response/transaction_test.exs
@@ -12,21 +12,21 @@ defmodule JSONRPC2.Response.TransactionTest do
 
       response_transaction = Transaction.new(internal_transaction, internal_block)
 
-      assert response_transaction == %Transaction{
-               blockHash: "0x0000000000000000000000000000000000000000000000000000000000000010",
-               blockNumber: "0x01",
-               transactionIndex: "0x00",
+      assert response_transaction == %JSONRPC2.Response.Transaction{
+               blockNumber: "0x1",
                from: "0x619f56e8bed07fe196c0dbc41b52e2bc64817b3a",
-               gas: "0x07",
-               gasPrice: "0x06",
-               hash: "0x7f71d14c13c402ce13c6f063b83e085f097a878e33163d6146cece277936531b",
-               input: "0x01",
-               nonce: "0x05",
+               gas: "0x7",
+               gasPrice: "0x6",
+               nonce: "0x5",
                r: "0x55fa77ee62e6c42e83b4f868c1e41643e45fd6f02a381a663318884751cb690a",
                s: "0x7bd63c407cea7d619d598fb5766980ab8497b1b11c26d8bc59a132af96317793",
-               to: "0x",
+               transactionIndex: "0x0",
                v: "0x1b",
-               value: "0x05"
+               value: "0x5",
+               blockHash: "0x0000000000000000000000000000000000000000000000000000000000000010",
+               hash: "0x7f71d14c13c402ce13c6f063b83e085f097a878e33163d6146cece277936531b",
+               input: "0x01",
+               to: "0x"
              }
     end
 
@@ -40,7 +40,7 @@ defmodule JSONRPC2.Response.TransactionTest do
         |> Jason.encode!()
 
       assert json_transaction ==
-               "{\"blockHash\":\"0x0000000000000000000000000000000000000000000000000000000000000010\",\"blockNumber\":\"0x01\",\"from\":\"0x619f56e8bed07fe196c0dbc41b52e2bc64817b3a\",\"gas\":\"0x07\",\"gasPrice\":\"0x06\",\"hash\":\"0x7f71d14c13c402ce13c6f063b83e085f097a878e33163d6146cece277936531b\",\"input\":\"0x01\",\"nonce\":\"0x05\",\"r\":\"0x55fa77ee62e6c42e83b4f868c1e41643e45fd6f02a381a663318884751cb690a\",\"s\":\"0x7bd63c407cea7d619d598fb5766980ab8497b1b11c26d8bc59a132af96317793\",\"to\":\"0x\",\"transactionIndex\":\"0x00\",\"v\":\"0x1b\",\"value\":\"0x05\"}"
+               "{\"blockHash\":\"0x0000000000000000000000000000000000000000000000000000000000000010\",\"blockNumber\":\"0x1\",\"from\":\"0x619f56e8bed07fe196c0dbc41b52e2bc64817b3a\",\"gas\":\"0x7\",\"gasPrice\":\"0x6\",\"hash\":\"0x7f71d14c13c402ce13c6f063b83e085f097a878e33163d6146cece277936531b\",\"input\":\"0x01\",\"nonce\":\"0x5\",\"r\":\"0x55fa77ee62e6c42e83b4f868c1e41643e45fd6f02a381a663318884751cb690a\",\"s\":\"0x7bd63c407cea7d619d598fb5766980ab8497b1b11c26d8bc59a132af96317793\",\"to\":\"0x\",\"transactionIndex\":\"0x0\",\"v\":\"0x1b\",\"value\":\"0x5\"}"
     end
   end
 end
