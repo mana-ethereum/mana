570224623588a1a78632b7fd7f13cb2b33353223
Ayrat Badykov <ayratin555@gmail.com>
2018-12-21 14:49:03 +0300

    fix release

--- apps/jsonrpc2/lib/bridge/sync.ex
+++ apps/jsonrpc2/lib/bridge/sync.ex
@@ -32,38 +32,16 @@ defmodule JSONRPC2.Bridge.Sync do
   @spec get_last_sync_state() :: Sync.state()
   defp get_last_sync_state(), do: Sync.get_state()
 
-  def get_block_by_number(number, include_full_transactions) do
+  def get_block(hash_or_number, include_full_transactions) do
     state_trie = get_last_sync_state().trie
 
-    case Block.get_block(number, state_trie) do
+    case Block.get_block(hash_or_number, state_trie) do
       {:ok, block} -> ResponseBlock.new(block, include_full_transactions)
       _ -> nil
     end
   end
 
-  def get_block_by_hash(hash, include_full_transactions) do
-    state_trie = get_last_sync_state().trie
-
-    case Block.get_block(hash, state_trie) do
-      {:ok, block} -> ResponseBlock.new(block, include_full_transactions)
-      _ -> nil
-    end
-  end
-
-  def get_transaction_by_block_hash_and_index(block_hash, trx_index) do
-    trie = get_last_sync_state().trie
-
-    with {:ok, block} <- Block.get_block(block_hash, trie) do
-      case Enum.at(block.transactions, trx_index) do
-        nil -> nil
-        transaction -> ResponseTransaction.new(transaction, block)
-      end
-    else
-      _ -> nil
-    end
-  end
-
-  def get_transaction_by_block_number_and_index(block_number, trx_index) do
+  def get_transaction_by_block_and_index(block_number, trx_index) do
     trie = get_last_sync_state().trie
 
     with {:ok, block} <- Block.get_block(block_number, trie) do
@@ -76,10 +54,10 @@ defmodule JSONRPC2.Bridge.Sync do
     end
   end
 
-  def get_block_transaction_count_by_number(number) do
+  def get_block_transaction_count(number_or_hash) do
     state_trie = get_last_sync_state().trie
 
-    case Block.get_block(number, state_trie) do
+    case Block.get_block(number_or_hash, state_trie) do
       {:ok, block} ->
         block.transactions
         |> Enum.count()
@@ -90,38 +68,10 @@ defmodule JSONRPC2.Bridge.Sync do
     end
   end
 
-  def get_block_transaction_count_by_hash(hash) do
+  def get_uncle_count(number_or_hash) do
     state_trie = get_last_sync_state().trie
 
-    case Block.get_block(hash, state_trie) do
-      {:ok, block} ->
-        block.transactions
-        |> Enum.count()
-        |> Exth.encode_unsigned_hex()
-
-      _ ->
-        nil
-    end
-  end
-
-  def get_uncle_count_by_block_hash(hash) do
-    state_trie = get_last_sync_state().trie
-
-    case Block.get_block(hash, state_trie) do
-      {:ok, block} ->
-        block.ommers
-        |> Enum.count()
-        |> Exth.encode_unsigned_hex()
-
-      _ ->
-        nil
-    end
-  end
-
-  def get_uncle_count_by_block_number(number) do
-    state_trie = get_last_sync_state().trie
-
-    case Block.get_block(number, state_trie) do
+    case Block.get_block(number_or_hash, state_trie) do
       {:ok, block} ->
         block.ommers
         |> Enum.count()
@@ -199,32 +149,10 @@ defmodule JSONRPC2.Bridge.Sync do
     end
   end
 
-  def get_uncle_by_block_hash_and_index(block_hash, index) do
+  def get_uncle(block_hash_or_number, index) do
     trie = get_last_sync_state().trie
 
-    case Block.get_block(block_hash, trie) do
-      {:ok, block} ->
-        case Enum.at(block.ommers, index) do
-          nil ->
-            nil
-
-          ommer_header ->
-            uncle_block = %Block{header: ommer_header, transactions: [], ommers: []}
-
-            uncle_block
-            |> Block.add_metadata(trie)
-            |> ResponseBlock.new()
-        end
-
-      _ ->
-        nil
-    end
-  end
-
-  def get_uncle_by_block_number_and_index(block_number, index) do
-    trie = get_last_sync_state().trie
-
-    case Block.get_block(block_number, trie) do
+    case Block.get_block(block_hash_or_number, trie) do
       {:ok, block} ->
         case Enum.at(block.ommers, index) do
           nil ->
